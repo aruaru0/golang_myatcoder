a5ca0084a16b13a390ea5a4a06cce91e042d6e8d
aruaru0 <aruaru0@gmail.com>
2020-10-20 20:11:00 +0900

    20101020

---
+++ yukicoder/000-999/305/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+const n = 10
+
+func put(a []int) {
+	for _, e := range a {
+		fmt.Print(e)
+	}
+	fmt.Println()
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, 1000000)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	ans := make([]int, n)
+	put(ans)
+	x, _ := getI(), getS()
+	if x == n {
+		return
+	}
+	for i := 0; i < n; i++ {
+		a := make([]int, n)
+		for j := 1; j < 10; j++ {
+			a[i] = j
+			put(a)
+			x0, _ := getI(), getS()
+			if x0 == 10 {
+				return
+			}
+			if x+1 == x0 {
+				ans[i] = j
+				break
+			}
+			if x == x0+1 {
+				ans[i] = 0
+				break
+			}
+		}
+	}
+	put(ans)
+}
---
+++ yukicoder/000-999/342/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanLines)
+	sc.Buffer([]byte{}, 1000000)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	S := getS()
+	m := make(map[int][]string)
+	s := ""
+	flg := false
+	cnt := 0
+	ma := 0
+	for _, e := range S {
+		if e == 'ｗ' {
+			flg = true
+			cnt++
+			continue
+		}
+		if flg == true {
+			if s != "" {
+				m[cnt] = append(m[cnt], s)
+				ma = max(ma, cnt)
+			}
+			s = ""
+			flg = false
+			cnt = 0
+		}
+		s = s + string(e)
+	}
+	if flg == true {
+		m[cnt] = append(m[cnt], s)
+		ma = max(ma, cnt)
+	}
+
+	for _, e := range m[ma] {
+		if e == "" {
+			continue
+		}
+		out(e)
+	}
+}

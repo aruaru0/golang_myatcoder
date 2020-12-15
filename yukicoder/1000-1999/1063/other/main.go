289c644fceed99e99b2c38189906b057ef866539
aruaru0 <aruaru0@gmail.com>
2020-12-15 21:43:02 +0900

    20201215

---
+++ yukicoder/1000-1999/1063/other/main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func pfs(n int) map[int]int {
+	ret := make(map[int]int)
+	for i := 2; i*i <= n; i++ {
+		for n%i == 0 {
+			ret[i]++
+			n /= i
+		}
+	}
+	if n != 1 {
+		ret[n]++
+	}
+	return ret
+}
+
+func pow(a, n int) int {
+	ret := 1
+	for n != 0 {
+		if n%2 == 1 {
+			ret *= a
+		}
+		a *= a
+		n /= 2
+	}
+	return ret
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, 1000000)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	n := getI()
+
+	p := pfs(n)
+	a := 1
+	b := 1
+	for i, e := range p {
+		if e%2 == 1 {
+			b *= i
+			e--
+		}
+		a *= pow(i, e/2)
+	}
+
+	out(a, b)
+}

c8ec1ac711e34ce8a880430a0edd2afda5d39a4b
aruaru0 <aruaru0@gmail.com>
2021-02-10 20:31:36 +0900

    20210210

---
+++ PM9/210209/C/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func f(s []byte) (int, []byte) {
+	// 3連続を変形
+	cnt := 0
+	for i := 1; i < len(s)-1; i++ {
+		if s[i-1] == s[i] && s[i] == s[i+1] {
+			s[i] = '.'
+			cnt++
+		}
+	}
+	// 2連続を変形
+	for i := 1; i < len(s); i++ {
+		if s[i-1] == s[i] {
+			s[i] = '.'
+			cnt++
+		}
+	}
+	return cnt, s
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	s := getS()
+	k := getI()
+
+	cs := [5]int{}
+	n := len(s)
+	sss := s + s + s + s + s
+	for i := 1; i < n*min(k, 5); i++ {
+		if sss[i-1] == sss[i] {
+			cs[i/n]++
+			i++
+		}
+	}
+	ans := cs[0] + (k/2)*cs[1] + ((k-1)/2)*cs[2]
+	out(ans)
+}

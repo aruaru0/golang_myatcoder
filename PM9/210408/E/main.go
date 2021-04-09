3d766e85c284b1d1b1292be412858f9e8b378735
aruaru0 <aruaru0@gmail.com>
2021-04-09 20:36:15 +0900

    20210409

---
+++ PM9/210408/E/main.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type pair struct {
+	c2, c5 int
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+	a := make([]int, N)
+	for i := 0; i < N; i++ {
+		s := getS()
+		x := strings.Split(s, ".")
+		var t string
+		if len(x) == 1 {
+			t = x[0] + "000000000"
+		} else {
+			t = x[0] + (x[1] + "000000000")[:9]
+		}
+		a[i], _ = strconv.Atoi(t)
+	}
+
+	b := make(map[pair]int)
+	for _, e := range a {
+		c2, c5 := 0, 0
+		for e%2 == 0 && c2 < 18 {
+			c2++
+			e /= 2
+		}
+		for e%5 == 0 && c5 < 18 {
+			c5++
+			e /= 5
+		}
+		b[pair{c2, c5}]++
+	}
+	// out(b)
+
+	ans := 0
+	for t1 := 0; t1 <= 18; t1++ {
+		for t2 := 0; t2 <= 18; t2++ {
+			for f1 := 0; f1 <= 18; f1++ {
+				for f2 := 0; f2 <= 18; f2++ {
+					if t1+t2 < 18 || f1+f2 < 18 {
+						continue
+					}
+					if t1 == t2 && f1 == f2 {
+						ans += b[pair{t1, f1}] * (b[pair{t1, f1}] - 1)
+					} else {
+						ans += b[pair{t1, f1}] * b[pair{t2, f2}]
+					}
+				}
+			}
+		}
+	}
+	ans /= 2
+	out(ans)
+}

24d1f239551368f606f60ea8a4440ab5685aa698
aruaru0 <aruaru0@gmail.com>
2021-04-20 20:32:24 +0900

    20210420

---
+++ PM9/210419/E/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+// kokoha ok
+func f(a []int, m int) int {
+	ret := 0
+	for _, x := range a {
+		if ret > int(1e18+1)/m {
+			return math.MaxInt64
+		}
+		ret = ret*m + x
+	}
+	return ret
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	X, M := getS(), getI()
+
+	a := make([]int, len(X))
+	d := 0
+	for i := 0; i < len(X); i++ {
+		a[i] = int(X[i] - '0')
+		d = max(d, a[i])
+	}
+
+	if len(X) == 1 {
+		if a[0] > M {
+			out(0)
+			return
+		}
+		out(1)
+		return
+	}
+
+	l := d
+	r := int(1e18 + 1)
+	for l+1 != r {
+		m := (l + r) / 2
+		// out(l, r, m, f(a, m))
+		if f(a, m) > M {
+			r = m
+		} else {
+			l = m
+		}
+	}
+
+	out(l - d)
+}

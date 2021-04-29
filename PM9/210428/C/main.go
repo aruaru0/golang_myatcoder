9fa15e2c5955db9a2687fa34b163c5fd82d593c7
aruaru0 <aruaru0@gmail.com>
2021-04-29 20:46:59 +0900

    20210429

---
+++ PM9/210428/C/main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	H, W := getI(), getI()
+	K := getI()
+	c := make([]string, H)
+	for i := 0; i < H; i++ {
+		c[i] = getS()
+	}
+
+	h := 1 << H
+	w := 1 << W
+	ans := 0
+	for i := 0; i < h; i++ {
+		y := make([]bool, H)
+		for j := 0; j < H; j++ {
+			if (i>>j)%2 == 1 {
+				y[j] = true
+			}
+		}
+		for j := 0; j < w; j++ {
+			x := make([]bool, W)
+			for k := 0; k < W; k++ {
+				if (j>>k)%2 == 1 {
+					x[k] = true
+				}
+			}
+			//---
+			cnt := 0
+			for yi, yy := range y {
+				for xi, xx := range x {
+					if !yy && !xx {
+						if c[yi][xi] == '#' {
+							cnt++
+						}
+					}
+				}
+			}
+			if cnt == K {
+				ans++
+			}
+		}
+	}
+
+	out(ans)
+}

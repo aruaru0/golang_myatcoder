5da43dfa85452a5e31d11e7d57bbaeff2714ecc1
aruaru0 <aruaru0@gmail.com>
2021-05-05 11:43:49 +0900

    20210505

---
+++ ARC/114/B/main.go
@@ -0,0 +1,458 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type SccGraph struct {
+	n     int
+	edges [][2]int
+}
+type Csr struct {
+	start []int
+	elist []int
+}
+
+func newSccGraph(n int) *SccGraph {
+	scc := new(SccGraph)
+	scc.n = n
+	return scc
+}
+func (scc *SccGraph) NumVertices() int {
+	return scc.n
+}
+func (scc *SccGraph) AddEdge(from int, to int) {
+	scc.edges = append(scc.edges, [2]int{from, to})
+}
+func (c *Csr) csr(n int, edges [][2]int) {
+	c.start = make([]int, n+1)
+	c.elist = make([]int, len(edges))
+	for _, e := range edges {
+		c.start[e[0]+1]++
+	}
+	for i := 1; i <= n; i++ {
+		c.start[i] += c.start[i-1]
+	}
+	counter := make([]int, n+1)
+	copy(counter, c.start)
+	for _, e := range edges {
+		c.elist[counter[e[0]]] = e[1]
+		counter[e[0]]++
+	}
+}
+func (scc *SccGraph) SccIds() (int, []int) {
+	g := new(Csr)
+	g.csr(scc.n, scc.edges)
+	nowOrd, groupNum := 0, 0
+	visited, low := make([]int, 0, scc.n), make([]int, scc.n)
+	ord, ids := make([]int, scc.n), make([]int, scc.n)
+	for i := 0; i < scc.n; i++ {
+		ord[i] = -1
+	}
+	var dfs func(v int)
+	dfs = func(v int) {
+		low[v], ord[v] = nowOrd, nowOrd
+		nowOrd++
+		visited = append(visited, v)
+		for i := g.start[v]; i < g.start[v+1]; i++ {
+			to := g.elist[i]
+			if ord[to] == -1 {
+				dfs(to)
+				low[v] = scc.min(low[v], low[to])
+			} else {
+				low[v] = scc.min(low[v], ord[to])
+			}
+		}
+		if low[v] == ord[v] {
+			for {
+				u := visited[len(visited)-1]
+				visited = visited[:len(visited)-1]
+				ord[u] = scc.n
+				ids[u] = groupNum
+				if u == v {
+					break
+				}
+			}
+			groupNum++
+		}
+	}
+	for i := 0; i < scc.n; i++ {
+		if ord[i] == -1 {
+			dfs(i)
+		}
+	}
+	for i := 0; i < len(ids); i++ {
+		ids[i] = groupNum - 1 - ids[i]
+	}
+	return groupNum, ids
+}
+func (scc *SccGraph) min(x, y int) int {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
+func (scc *SccGraph) Scc() [][]int {
+	groupNum, ids := scc.SccIds()
+	counts := make([]int, groupNum)
+	for _, x := range ids {
+		counts[x]++
+	}
+	groups := make([][]int, groupNum)
+	for i := 0; i < groupNum; i++ {
+		groups[i] = make([]int, 0, counts[i])
+	}
+	for i := 0; i < scc.n; i++ {
+		groups[ids[i]] = append(groups[ids[i]], i)
+	}
+	return groups
+}
+
+//----------------------------------------
+// modint
+//----------------------------------------
+type modint struct {
+	mod       int
+	factMemo  []int
+	ifactMemo []int
+}
+
+func newModint(m int) *modint {
+	var ret modint
+	ret.mod = m
+	ret.factMemo = []int{1, 1}
+	ret.ifactMemo = []int{1, 1}
+	return &ret
+}
+
+func (m *modint) add(a, b int) int {
+	ret := (a + b) % m.mod
+	if ret < 0 {
+		ret += m.mod
+	}
+	return ret
+}
+
+func (m *modint) sub(a, b int) int {
+	ret := (a - b) % m.mod
+	if ret < 0 {
+		ret += m.mod
+	}
+	return ret
+}
+
+func (m *modint) mul(a, b int) int {
+	ret := a * b % m.mod
+	if ret < 0 {
+		ret += m.mod
+	}
+	return ret
+}
+
+func (m *modint) div(a, b int) int {
+	ret := a * m.modinv(b)
+	ret %= m.mod
+	return ret
+}
+
+func (m *modint) pow(p, n int) int {
+	ret := 1
+	x := p
+	for n != 0 {
+		if n%2 == 1 {
+			ret *= x
+			ret %= m.mod
+		}
+		n /= 2
+		x = x * x % m.mod
+	}
+	return ret
+}
+
+// 逆元を使った割り算（MOD）
+// mod. m での a の逆元 a^{-1} を計算する
+// func (m *modint) modinv(a int) int {
+// 	b := m.mod
+// 	u := 1
+// 	v := 0
+// 	for b != 0 {
+// 		t := a / b
+// 		a -= t * b
+// 		a, b = b, a
+// 		u -= t * v
+// 		u, v = v, u
+// 	}
+// 	u %= m.mod
+// 	if u < 0 {
+// 		u += m.mod
+// 	}
+// 	return u
+// }
+
+// 拡張オイラーの互除法で逆元を求める
+func (mm *modint) modinv(a int) int {
+	m := mm.mod
+	b, u, v := m, 1, 0
+	for b != 0 {
+		t := a / b
+		a -= t * b
+		a, b = b, a
+		u -= t * v
+		u, v = v, u
+	}
+	u %= m
+	if u < 0 {
+		u += m
+	}
+	return u
+}
+
+//-----------------------------------------------
+// 行列累乗
+// 　A[][]のp乗を求める
+//-----------------------------------------------
+func (m *modint) powModMatrix(A [][]int, p int) [][]int {
+	N := len(A)
+	ret := make([][]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = make([]int, N)
+		ret[i][i] = 1
+	}
+
+	for p > 0 {
+		if p&1 == 1 {
+			ret = m.mulMod(ret, A)
+		}
+		A = m.mulMod(A, A)
+		p >>= 1
+	}
+
+	return ret
+}
+
+func (m *modint) mulMod(A, B [][]int) [][]int {
+	H := len(A)
+	W := len(B[0])
+	K := len(A[0])
+	C := make([][]int, W)
+	for i := 0; i < W; i++ {
+		C[i] = make([]int, W)
+	}
+
+	for i := 0; i < H; i++ {
+		for j := 0; j < W; j++ {
+			for k := 0; k < K; k++ {
+				C[i][j] += A[i][k] * B[k][j]
+				C[i][j] %= m.mod
+			}
+		}
+	}
+
+	return C
+}
+
+//---------------------------------------------------
+// nCk 計算関連：　TELすることがあるかも
+//                ※pow(x, p-2)を何度も取るので
+// 厳しそうな場合は、ここを削除して高速なのを使う
+//---------------------------------------------------
+func (m *modint) mfact(n int) int {
+	if len(m.factMemo) > n {
+		return m.factMemo[n]
+	}
+	if len(m.factMemo) == 0 {
+		m.factMemo = append(m.factMemo, 1)
+	}
+	for len(m.factMemo) <= n {
+		size := len(m.factMemo)
+		m.factMemo = append(m.factMemo, m.factMemo[size-1]*size%m.mod)
+	}
+	return m.factMemo[n]
+}
+
+func (m *modint) mifact(n int) int {
+	if len(m.ifactMemo) > n {
+		return m.ifactMemo[n]
+	}
+	if len(m.ifactMemo) == 0 {
+		m.factMemo = append(m.ifactMemo, 1)
+	}
+	for len(m.ifactMemo) <= n {
+		size := len(m.ifactMemo)
+		m.ifactMemo = append(m.ifactMemo, m.ifactMemo[size-1]*m.pow(size, m.mod-2)%m.mod)
+	}
+	return m.ifactMemo[n]
+}
+
+func (m *modint) nCr(n, r int) int {
+	if n == r {
+		return 1
+	}
+	if n < r || r < 0 {
+		return 0
+	}
+	ret := 1
+	ret *= m.mfact(n)
+	ret %= m.mod
+	ret *= m.mifact(r)
+	ret %= m.mod
+	ret *= m.mifact(n - r)
+	ret %= m.mod
+	return (ret)
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+	f := make([]int, N)
+	for i := 0; i < N; i++ {
+		x := getI() - 1
+		f[i] = x
+	}
+
+	scc := newSccGraph(N)
+	for i := 0; i < N; i++ {
+		scc.AddEdge(i, f[i])
+	}
+
+	s := scc.Scc()
+	cnt := 0
+	for _, e := range s {
+		if len(e) == 1 && f[e[0]] != e[0] {
+			continue
+		}
+		cnt++
+	}
+
+	mod := newModint(998244353)
+	ans := 0
+	for i := 1; i <= cnt; i++ {
+		ans = mod.add(ans, mod.nCr(cnt, i))
+	}
+	out(ans)
+}
---
+++ ARC/114/B/other/main.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+var N int
+var f []int
+var used []int
+
+func dfs(cur int) bool {
+	used[cur] = 1
+	ret := false
+	if used[f[cur]] == 0 {
+		ret = ret || dfs(f[cur])
+	} else if used[f[cur]] == 1 {
+		ret = true
+	}
+	used[cur] = 2
+	return ret
+}
+
+const mod = 998244353
+
+func modpow(x, n int) int {
+	ret := 1
+	for n > 0 {
+		if n%2 == 1 {
+			ret *= x
+			ret %= mod
+		}
+		n /= 2
+		x *= x
+		x %= mod
+	}
+	return ret
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N = getI()
+	f = make([]int, N)
+	for i := 0; i < N; i++ {
+		f[i] = getI() - 1
+	}
+
+	used = make([]int, N)
+	cnt := 0
+	for i := 0; i < N; i++ {
+		if used[i] == 0 {
+			if dfs(i) {
+				cnt++
+			}
+		}
+	}
+
+	ans := modpow(2, cnt)
+	ans = (ans - 1 + mod) % mod
+	out(ans)
+}

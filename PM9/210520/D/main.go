fa92b56bf5d07bcee38f9ed1901f787dccb7fb9f
aruaru0 <aruaru0@gmail.com>
2021-05-21 20:28:45 +0900

    20210520

---
+++ PM9/210520/D/main.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	H, W, N, M := getI(), getI(), getI(), getI()
+
+	d := make([][]byte, H)
+	for i := 0; i < H; i++ {
+		d[i] = make([]byte, W)
+		for j := 0; j < W; j++ {
+			d[i][j] = '.'
+		}
+	}
+	for i := 0; i < N; i++ {
+		r, c := getI()-1, getI()-1
+		d[r][c] = 'o'
+	}
+	for i := 0; i < M; i++ {
+		r, c := getI()-1, getI()-1
+		d[r][c] = 'x'
+	}
+
+	for r := 0; r < H; r++ {
+		val := byte('.')
+		for c := 0; c < W; c++ {
+			if d[r][c] == 'o' {
+				val = '-'
+			} else if d[r][c] == 'x' {
+				val = '.'
+			} else if d[r][c] == '.' {
+				d[r][c] = val
+			}
+		}
+		val = '.'
+		for c := W - 1; c >= 0; c-- {
+			if d[r][c] == 'o' {
+				val = '-'
+			} else if d[r][c] == 'x' {
+				val = '.'
+			} else if d[r][c] == '.' {
+				d[r][c] = val
+			}
+		}
+	}
+
+	for c := 0; c < W; c++ {
+		val := byte('.')
+		for r := 0; r < H; r++ {
+			if d[r][c] == 'o' {
+				val = '-'
+			} else if d[r][c] == 'x' {
+				val = '.'
+			} else if d[r][c] == '.' {
+				d[r][c] = val
+			}
+		}
+		val = '.'
+		for r := H - 1; r >= 0; r-- {
+			if d[r][c] == 'o' {
+				val = '-'
+			} else if d[r][c] == 'x' {
+				val = 0
+			} else if d[r][c] == '.' {
+				d[r][c] = val
+			}
+		}
+	}
+
+	cnt := 0
+	for r := 0; r < H; r++ {
+		for c := 0; c < W; c++ {
+			if d[r][c] == '-' || d[r][c] == 'o' {
+				cnt++
+			}
+		}
+	}
+	out(cnt)
+}

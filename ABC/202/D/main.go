8d3c2b6db69ccb505cb1e78eb050e115b7ab55ea
aruaru0 <aruaru0@gmail.com>
2021-05-23 09:55:15 +0900

    20210522

---
+++ ABC/202/D/main.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func NextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+var dp [31][31]int
+
+func f(a, b, k int) string {
+	if a == 0 {
+		s := ""
+		for i := 0; i < b; i++ {
+			s += "b"
+		}
+		return s
+	}
+	if b == 0 {
+		s := ""
+		for i := 0; i < a; i++ {
+			s += "a"
+		}
+		return s
+	}
+	if k <= dp[a-1][b] {
+		return "a" + f(a-1, b, k)
+	} else {
+		return "b" + f(a, b-1, k-dp[a-1][b])
+	}
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	// A, B, K := getI(), getI(), getI()
+	A, B, K := getI(), getI(), getI()
+
+	for a := 0; a <= 30; a++ {
+		for b := 0; b <= 30; b++ {
+			tot := 1
+			n := a + b
+			r := b
+			for i := 1; i <= r; i++ {
+				tot *= (n + 1 - i)
+				tot /= i
+			}
+			dp[a][b] = tot
+		}
+	}
+
+	out(f(A, B, K))
+}
---
+++ ABC/202/E/main.go
@@ -0,0 +1,355 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+/*
+Lowest Common Ansestor
+*/
+type lca struct {
+	G     [][]int
+	vs    []int
+	depth []int
+	id    []int
+	n     int
+	k     int
+	seg   *SegmentTree
+}
+
+const inf = int(1e16)
+
+func newLCA(root, n int, node [][]int) *lca {
+	var l lca
+	l.n = n
+	l.G = node
+	l.vs = make([]int, n*2-1)
+	l.depth = make([]int, n*2-1)
+	l.id = make([]int, n)
+	l.k = 0
+	l.dfs(root, -1, 0)
+
+	l.seg = SegtreeInit(n*2-1, Data{inf, 0})
+	for i, e := range l.depth {
+		l.seg.Set(i, Data{e, i})
+	}
+	l.seg.Update()
+	return &l
+}
+
+func (l *lca) lca(u, v int) int {
+	idx := l.seg.Query(min(l.id[u], l.id[v]), max(l.id[u], l.id[v]))
+	return l.vs[idx.idx]
+}
+
+func (l *lca) dfs(v, p, d int) {
+	l.id[v] = l.k
+	l.vs[l.k] = v
+	l.depth[l.k] = d
+	l.k++
+	// out(v, p, l.G[v], l.k)
+	for i := 0; i < len(l.G[v]); i++ {
+		if l.G[v][i] != p {
+			l.dfs(l.G[v][i], v, d+1)
+			l.vs[l.k] = v
+			l.depth[l.k] = d
+			l.k++
+		}
+	}
+}
+
+/*
+  セグメント木(2020.05.24作成)
+	SegtreeInitで初期化
+	Setで値設定し、Updateで木作成
+	Getで値を取得
+	UpdateAtで個別のアイテムを更新
+	Queryで区間の値を取得
+	compareで比較方法変更可能
+*/
+
+// Data :
+// Data型をstructすれば複数データが持てる
+// compareも変更すること！
+type Data struct {
+	val, idx int
+}
+
+// SegmentTree :
+type SegmentTree struct {
+	inf    Data
+	d      []Data
+	offset int
+}
+
+// SegtreeInit :　nが要素数、valが初期値
+func SegtreeInit(n int, val Data) *SegmentTree {
+	var ret SegmentTree
+	size := 1
+	for size < n {
+		size *= 2
+	}
+	ret.d = make([]Data, size*2)
+	for i := 1; i < size*2; i++ {
+		ret.d[i] = val
+	}
+	ret.offset = size
+	ret.inf = val
+	return &ret
+}
+
+// Set : 要素に値をセット（※木は更新されない）
+func (s *SegmentTree) Set(idx int, val Data) {
+	s.d[s.offset+idx] = val
+}
+
+// Get : 要素に値を取得
+func (s *SegmentTree) Get(idx int) Data {
+	return s.d[s.offset+idx]
+}
+
+// Update :
+func (s *SegmentTree) Update() {
+	N := s.offset
+	off := s.offset
+
+	for N > 1 {
+		for i := off; i < off+N; i += 2 {
+			p := i / 2
+			l := i
+			r := i + 1
+			s.d[p] = s.compare(s.d[l], s.d[r])
+		}
+		off /= 2
+		N /= 2
+	}
+}
+
+// querySub :
+// a, b ... 範囲
+func (s *SegmentTree) querySub(a, b, k, l, r int) Data {
+	if r <= a || b <= l {
+		return s.inf
+	}
+	if a <= l && r <= b {
+		return s.d[k]
+	}
+	return s.compare(
+		s.querySub(a, b, k*2, l, (l+r)/2),
+		s.querySub(a, b, k*2+1, (l+r)/2, r))
+}
+
+// Query :
+// a, b ... 範囲 a <= x < bの範囲で検索
+// [a, b)となっているのに注意
+func (s *SegmentTree) Query(a, b int) Data {
+	return s.querySub(a, b, 1, 0, s.offset)
+}
+
+// UpdateAt :
+func (s *SegmentTree) UpdateAt(n int, val Data) {
+	pos := s.offset + n
+	s.d[pos] = val
+	for pos > 1 {
+		p := pos / 2
+		l := p * 2
+		r := p*2 + 1
+		s.d[p] = s.compare(s.d[l], s.d[r])
+		pos /= 2
+	}
+}
+
+// compare :
+// 比較関数（ここで比較方法を設定）
+// ※min,maxを入れ替えるときなどは、Initの設定注意
+func (s *SegmentTree) compare(l, r Data) Data {
+	// 区間の合計の場合はinitを0にして下記
+	// return l + r
+
+	// 区間のminの場合はinfに最大値以上を設定して下記
+	if l.val < r.val {
+		return l
+	}
+	return r
+}
+
+func dfs(c, p, cnt int) {
+	dist[c] = cnt
+	for _, e := range node[c] {
+		if e == p {
+			continue
+		}
+		dfs(e, c, cnt+1)
+	}
+}
+
+var node [][]int
+var dist []int
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+	p := getInts(N - 1)
+	node = make([][]int, N)
+	for i := 0; i < N-1; i++ {
+		f := p[i] - 1
+		t := i + 1
+		node[f] = append(node[f], t)
+		node[t] = append(node[t], f)
+	}
+
+	dist = make([]int, N)
+	dfs(0, -1, 0)
+	// out(dist)
+
+	m := make(map[int][]int)
+	for i := 0; i < N; i++ {
+		m[dist[i]] = append(m[dist[i]], i)
+	}
+	// out(m)
+
+	l := newLCA(0, N, node)
+
+	Q := getI()
+	for i := 0; i < Q; i++ {
+		u, d := getI()-1, getI()
+		tot := 0
+		// out(d, u, dist[u])
+		for _, e := range m[d] {
+			if u == e {
+				tot++
+				continue
+			}
+			x := l.lca(u, e)
+			// out(u, e, "lca", x, x == u)
+			if x == u {
+				tot++
+			}
+		}
+		out(tot)
+	}
+}

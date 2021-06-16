de0abe33cb6180aeef5980eab2d39873dd0d9c08
aruaru0 <aruaru0@gmail.com>
2021-06-16 20:19:22 +0900

    20210615

---
+++ PM9/210615/B/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type pos struct {
+	x, y int
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	m := make(map[int]pos)
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			m[getI()] = pos{i, j}
+		}
+	}
+	N := getI()
+	b := getInts(N)
+	var d [3][3]bool
+	for i := 0; i < N; i++ {
+		p, ok := m[b[i]]
+		if ok {
+			d[p.x][p.y] = true
+		}
+	}
+	ok := false
+	for i := 0; i < 3; i++ {
+		if d[i][0] && d[i][1] && d[i][2] {
+			ok = true
+		}
+		if d[0][i] && d[1][i] && d[2][i] {
+			ok = true
+		}
+	}
+	if d[0][0] && d[1][1] && d[2][2] {
+		ok = true
+	}
+	if d[2][0] && d[1][1] && d[0][2] {
+		ok = true
+	}
+	if ok {
+		out("Yes")
+	} else {
+		out("No")
+	}
+}

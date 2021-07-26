6aa9ab09e3f6394427978683604075d41d9ee927
aruaru0 <aruaru0@gmail.com>
2021-07-26 21:04:33 +0900

    210726

---
+++ ABC/211/D/main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+const inf = int(1e18)
+const mod = int(1e9 + 7)
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, M := getI(), getI()
+	node := make([][]int, N)
+	for i := 0; i < M; i++ {
+		from, to := getI()-1, getI()-1
+		node[from] = append(node[from], to)
+		node[to] = append(node[to], from)
+	}
+
+	cnt := make([]int, N)
+	dist := make([]int, N)
+	for i := 0; i < N; i++ {
+		dist[i] = inf
+	}
+	q := []int{0}
+	dist[0] = 0
+	cnt[0] = 1
+	for len(q) != 0 {
+		c := q[0]
+		q = q[1:]
+		for _, e := range node[c] {
+			if dist[e] == dist[c]+1 {
+				cnt[e] += cnt[c]
+				cnt[e] %= mod
+			}
+			if dist[e] > dist[c]+1 {
+				dist[e] = dist[c] + 1
+				cnt[e] = cnt[c] % mod
+				q = append(q, e)
+			}
+		}
+	}
+	out(cnt[N-1])
+}

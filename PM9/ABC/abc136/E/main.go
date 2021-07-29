bc8e567cc8aebc0b4b747f32ad403821e399f35d
aruaru0 <aruaru0@gmail.com>
2021-07-29 21:09:01 +0900

    210729

---
+++ PM9/ABC/abc136/E/main.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, K := getI(), getI()
+	a := getInts(N)
+
+	f := func(x int) bool {
+		r := make([]int, N)
+		for i := 0; i < N; i++ {
+			r[i] = a[i] % x
+		}
+		sort.Ints(r)
+		// 累積和
+		rsum := make([]int, N+1)
+		for i := 0; i < N; i++ {
+			rsum[i+1] = rsum[i] + r[i]
+		}
+		for i := 0; i <= N && rsum[i] <= K; i++ {
+			// 足す方と引く法の総和が釣り合っている
+			if rsum[i] == (N-i)*x-(rsum[N]-rsum[i]) {
+				return true
+			}
+		}
+		return false
+	}
+
+	ans := 1
+	s := 0
+	for i := 0; i < N; i++ {
+		s += a[i]
+	}
+	for i := 1; i*i <= s; i++ {
+		if s%i != 0 {
+			continue
+		}
+		if f(i) {
+			ans = max(ans, i)
+		}
+		if f(s / i) {
+			ans = max(ans, s/i)
+		}
+	}
+	out(ans)
+}

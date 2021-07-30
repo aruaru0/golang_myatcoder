16001b5eddeed31ca3d8ec9b8906d3e18b8f6bca
aruaru0 <aruaru0@gmail.com>
2021-07-30 21:00:22 +0900

    210730

--- PM9/ABC/abc136/F/main.go
+++ PM9/ABC/abc136/F/main.go
@@ -124,6 +124,239 @@ func upperBound(a []int, x int) int {
 	return idx
 }
 
+type BIT struct {
+	v []int
+}
+
+func newBIT(n int) *BIT {
+	b := new(BIT)
+	b.v = make([]int, n)
+	return b
+}
+func (b BIT) sum(a int) int {
+	ret := 0
+	for i := a + 1; i > 0; i -= i & -i {
+		ret += b.v[i-1]
+	}
+	return ret
+}
+func (b BIT) rangeSum(x, y int) int {
+	if y == 0 {
+		return 0
+	}
+	y--
+	if x == 0 {
+		return b.sum(y)
+	} else {
+		return b.sum(y) - b.sum(x-1)
+	}
+}
+func (b BIT) add(a, w int) {
+	n := len(b.v)
+	for i := a + 1; i <= n; i += i & -i {
+		b.v[i-1] += w
+	}
+}
+
+//----------------------------------------
+// modint
+//----------------------------------------
+type modint struct {
+	mod       int
+	factMemo  []int
+	ifactMemo []int
+}
+
+func newModint(m int) *modint {
+	var ret modint
+	ret.mod = m
+	ret.factMemo = []int{1, 1}
+	ret.ifactMemo = []int{1, 1}
+	return &ret
+}
+
+func (m *modint) add(a, b int) int {
+	ret := (a + b) % m.mod
+	if ret < 0 {
+		ret += m.mod
+	}
+	return ret
+}
+
+func (m *modint) sub(a, b int) int {
+	ret := (a - b) % m.mod
+	if ret < 0 {
+		ret += m.mod
+	}
+	return ret
+}
+
+func (m *modint) mul(a, b int) int {
+	ret := a * b % m.mod
+	if ret < 0 {
+		ret += m.mod
+	}
+	return ret
+}
+
+func (m *modint) div(a, b int) int {
+	ret := a * m.modinv(b)
+	ret %= m.mod
+	return ret
+}
+
+func (m *modint) pow(p, n int) int {
+	ret := 1
+	x := p
+	for n != 0 {
+		if n%2 == 1 {
+			ret *= x
+			ret %= m.mod
+		}
+		n /= 2
+		x = x * x % m.mod
+	}
+	return ret
+}
+
+// 逆元を使った割り算（MOD）
+// mod. m での a の逆元 a^{-1} を計算する
+// func (m *modint) modinv(a int) int {
+// 	b := m.mod
+// 	u := 1
+// 	v := 0
+// 	for b != 0 {
+// 		t := a / b
+// 		a -= t * b
+// 		a, b = b, a
+// 		u -= t * v
+// 		u, v = v, u
+// 	}
+// 	u %= m.mod
+// 	if u < 0 {
+// 		u += m.mod
+// 	}
+// 	return u
+// }
+
+// 拡張オイラーの互除法で逆元を求める
+func (mm *modint) modinv(a int) int {
+	m := mm.mod
+	b, u, v := m, 1, 0
+	for b != 0 {
+		t := a / b
+		a -= t * b
+		a, b = b, a
+		u -= t * v
+		u, v = v, u
+	}
+	u %= m
+	if u < 0 {
+		u += m
+	}
+	return u
+}
+
+//-----------------------------------------------
+// 行列累乗
+// 　A[][]のp乗を求める
+//-----------------------------------------------
+func (m *modint) powModMatrix(A [][]int, p int) [][]int {
+	N := len(A)
+	ret := make([][]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = make([]int, N)
+		ret[i][i] = 1
+	}
+
+	for p > 0 {
+		if p&1 == 1 {
+			ret = m.mulMod(ret, A)
+		}
+		A = m.mulMod(A, A)
+		p >>= 1
+	}
+
+	return ret
+}
+
+func (m *modint) mulMod(A, B [][]int) [][]int {
+	H := len(A)
+	W := len(B[0])
+	K := len(A[0])
+	C := make([][]int, W)
+	for i := 0; i < W; i++ {
+		C[i] = make([]int, W)
+	}
+
+	for i := 0; i < H; i++ {
+		for j := 0; j < W; j++ {
+			for k := 0; k < K; k++ {
+				C[i][j] += A[i][k] * B[k][j]
+				C[i][j] %= m.mod
+			}
+		}
+	}
+
+	return C
+}
+
+//---------------------------------------------------
+// nCk 計算関連：　TELすることがあるかも
+//                ※pow(x, p-2)を何度も取るので
+// 厳しそうな場合は、ここを削除して高速なのを使う
+//---------------------------------------------------
+func (m *modint) mfact(n int) int {
+	if len(m.factMemo) > n {
+		return m.factMemo[n]
+	}
+	if len(m.factMemo) == 0 {
+		m.factMemo = append(m.factMemo, 1)
+	}
+	for len(m.factMemo) <= n {
+		size := len(m.factMemo)
+		m.factMemo = append(m.factMemo, m.factMemo[size-1]*size%m.mod)
+	}
+	return m.factMemo[n]
+}
+
+func (m *modint) mifact(n int) int {
+	if len(m.ifactMemo) > n {
+		return m.ifactMemo[n]
+	}
+	if len(m.ifactMemo) == 0 {
+		m.factMemo = append(m.ifactMemo, 1)
+	}
+	for len(m.ifactMemo) <= n {
+		size := len(m.ifactMemo)
+		m.ifactMemo = append(m.ifactMemo, m.ifactMemo[size-1]*m.pow(size, m.mod-2)%m.mod)
+	}
+	return m.ifactMemo[n]
+}
+
+func (m *modint) nCr(n, r int) int {
+	if n == r {
+		return 1
+	}
+	if n < r || r < 0 {
+		return 0
+	}
+	ret := 1
+	ret *= m.mfact(n)
+	ret %= m.mod
+	ret *= m.mifact(r)
+	ret %= m.mod
+	ret *= m.mifact(n - r)
+	ret %= m.mod
+	return (ret)
+}
+
+type pos struct {
+	x, y, i int
+}
+
+const inf = int(1e10)
+
 func main() {
 	defer wr.Flush()
 	sc.Split(bufio.ScanWords)
@@ -133,46 +366,70 @@ func main() {
 	N := getI()
 	x := make([]int, N)
 	y := make([]int, N)
+	p := make([]pos, N)
 	for i := 0; i < N; i++ {
 		x[i], y[i] = getI(), getI()
+		p[i] = pos{x[i], y[i], i}
 	}
 
-	f := func(n int) []int {
-		ret := make([]int, 4)
-		for i := 0; i < N; i++ {
-			if i == n {
-				continue
-			}
-			if x[i] > x[n] && y[i] > y[n] {
-				ret[0]++
-			}
-			if x[i] < x[n] && y[i] > y[n] {
-				ret[1]++
-			}
-			if x[i] < x[n] && y[i] < y[n] {
-				ret[2]++
-			}
-			if x[i] > x[n] && y[i] < y[n] {
-				ret[3]++
-			}
-		}
-		return ret
-	}
+	sort.Ints(x)
+	sort.Ints(y)
+	sort.Slice(p, func(i, j int) bool {
+		return p[i].x < p[j].x
+	})
 
-	tot := 0
-	all := 1<<N - 1
+	my := make(map[int]int)
+
+	pos := 0
 	for i := 0; i < N; i++ {
-		r := f(i)
+		_, ok := my[y[i]]
+		if !ok {
+			my[y[i]] = pos
+			pos++
+		}
+	}
+
+	bit := newBIT(len(my))
+	d := make([][4]int, N)
+	for i := 0; i < N; i++ {
+		cy := my[p[i].y]
+		cnt := bit.sum(cy)
+		bit.add(cy, 1)
+		d[i][1] = i - cnt
+		d[i][2] = cnt
+	}
+	bit = newBIT(len(my))
+	for i := N - 1; i >= 0; i-- {
+		cy := my[p[i].y]
+		cnt := bit.sum(cy)
+		bit.add(cy, 1)
+		d[i][0] = N - 1 - i - cnt
+		d[i][3] = cnt
+	}
+
+	mod := newModint(998244353)
+	pow2 := make([]int, N+1)
+	pow2[0] = 1
+	for i := 0; i < N; i++ {
+		pow2[i+1] = mod.mul(pow2[i], 2)
+	}
+	tot := 0
+	all := mod.sub(pow2[N], 1)
+	for i := 0; i < N; i++ {
+		r := d[i]
 		add := 0
 		for j := 0; j < 4; j++ {
 			if r[j] == 0 {
 				continue
 			}
-			add += 1<<r[j] - 1
+			add += mod.sub(pow2[r[j]], 1)
+			add %= mod.mod
 		}
-		sub := 1<<(r[0]+r[1]) + 1<<(r[1]+r[2]) + 1<<(r[2]+r[3]) + 1<<(r[3]+r[0]) - 4
-		out(r, sub, add)
-		tot += all - sub + add
+		sub := mod.sub(pow2[r[0]+r[1]]+pow2[r[1]+r[2]]+pow2[r[2]+r[3]]+pow2[r[3]+r[0]], 4)
+		// out(r, all, sub, add, mod.add(mod.sub(all, sub), add))
+		tot += mod.add(mod.sub(all, sub), add)
+		tot %= mod.mod
 	}
+
 	out(tot)
 }

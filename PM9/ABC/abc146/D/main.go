4db4fd5e9ada8f2615cf710fde2150bc4ff985fa
aruaru0 <aruaru0@gmail.com>
2021-08-20 21:01:35 +0900

    20210820

---
+++ PM9/ABC/abc146/D/main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+var node [][]edge
+var col []int
+
+const inf = int(1e10)
+
+func dfs(cur, prev, c int) {
+	// if used[cur] {
+	// 	return
+	// }
+	// used[cur] = true
+	cnt := 0
+	for _, e := range node[cur] {
+		if e.to == prev {
+			continue
+		}
+		if cnt == c {
+			cnt++
+		}
+		col[e.idx] = cnt
+		dfs(e.to, cur, cnt)
+		cnt++
+	}
+}
+
+type edge struct {
+	to, idx int
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+	node = make([][]edge, N)
+	for i := 0; i < N-1; i++ {
+		a, b := getI()-1, getI()-1
+		node[a] = append(node[a], edge{b, i})
+		node[b] = append(node[b], edge{a, i})
+	}
+
+	col = make([]int, N-1)
+	dfs(0, -1, inf)
+
+	m := make(map[int]bool)
+	for i := 0; i < N-1; i++ {
+		m[col[i]] = true
+	}
+	out(len(m))
+	for i := 0; i < N-1; i++ {
+		out(col[i] + 1)
+	}
+}

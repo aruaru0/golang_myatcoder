5798708aff06124ad19b1c086b16cb1e57aa75e2
aruaru0 <aruaru0@gmail.com>
2021-08-21 21:06:23 +0900

    20210821

--- PM9/ABC/abc146/F/main.go
+++ PM9/ABC/abc146/F/main.go
@@ -124,37 +124,203 @@ func upperBound(a []int, x int) int {
 	return idx
 }
 
+type E func() int
+type Merger func(a, b int) int
+type Compare func(v int) bool
+type Segtree struct {
+	n      int
+	size   int
+	log    int
+	d      []int
+	e      E
+	merger Merger
+}
+
+func newSegtree(v []int, e E, m Merger) *Segtree {
+	seg := new(Segtree)
+	seg.n = len(v)
+	seg.log = seg.ceilPow2(seg.n)
+	seg.size = 1 << uint(seg.log)
+	seg.d = make([]int, 2*seg.size)
+	seg.e = e
+	seg.merger = m
+	for i, _ := range seg.d {
+		seg.d[i] = seg.e()
+	}
+	for i := 0; i < seg.n; i++ {
+		seg.d[seg.size+i] = v[i]
+	}
+	for i := seg.size - 1; i >= 1; i-- {
+		seg.Update(i)
+	}
+	return seg
+}
+func (seg *Segtree) Update(k int) {
+	seg.d[k] = seg.merger(seg.d[2*k], seg.d[2*k+1])
+}
+func (seg *Segtree) Set(p, x int) {
+	p += seg.size
+	seg.d[p] = x
+	for i := 1; i <= seg.log; i++ {
+		seg.Update(p >> uint(i))
+	}
+}
+func (seg *Segtree) Get(p int) int {
+	return seg.d[p+seg.size]
+}
+func (seg *Segtree) Prod(l, r int) int {
+	sml, smr := seg.e(), seg.e()
+	l += seg.size
+	r += seg.size
+	for l < r {
+		if (l & 1) == 1 {
+			sml = seg.merger(sml, seg.d[l])
+			l++
+		}
+		if (r & 1) == 1 {
+			r--
+			smr = seg.merger(seg.d[r], smr)
+		}
+		l >>= 1
+		r >>= 1
+	}
+	return seg.merger(sml, smr)
+}
+func (seg *Segtree) AllProd() int {
+	return seg.d[1]
+}
+func (seg *Segtree) MaxRight(l int, cmp Compare) int {
+	if l == seg.n {
+		return seg.n
+	}
+	l += seg.size
+	sm := seg.e()
+	for {
+		for l%2 == 0 {
+			l >>= 1
+		}
+		if !cmp(seg.merger(sm, seg.d[l])) {
+			for l < seg.size {
+				l = 2 * l
+				if cmp(seg.merger(sm, seg.d[l])) {
+					sm = seg.merger(sm, seg.d[l])
+					l++
+				}
+			}
+			return l - seg.size
+		}
+		sm = seg.merger(sm, seg.d[l])
+		l++
+		if l&-l == l {
+			break
+		}
+	}
+	return seg.n
+}
+func (seg *Segtree) MinLeft(r int, cmp Compare) int {
+	if r == 0 {
+		return 0
+	}
+	r += seg.size
+	sm := seg.e()
+	for {
+		r--
+		for r > 1 && r%2 != 0 {
+			r >>= 1
+		}
+		if !cmp(seg.merger(seg.d[r], sm)) {
+			for r < seg.size {
+				r = 2*r + 1
+				if cmp(seg.merger(seg.d[r], sm)) {
+					sm = seg.merger(seg.d[r], sm)
+					r--
+				}
+			}
+			return r + 1 - seg.size
+		}
+		sm = seg.merger(seg.d[r], sm)
+		if r&-r == r {
+			break
+		}
+	}
+	return 0
+}
+func (seg *Segtree) ceilPow2(n int) int {
+	x := 0
+	for (1 << uint(x)) < n {
+		x++
+	}
+	return x
+}
+
+const inf = int(1e10)
+
+type pos struct {
+	v, i int
+}
+
 func main() {
 	defer wr.Flush()
 	sc.Split(bufio.ScanWords)
 	sc.Buffer([]byte{}, math.MaxInt32)
 	// this template is new version.
 	// use getI(), getS(), getInts(), getF()
-	N, K := getI(), getI()
+	N, M := getI(), getI()
+	s := getS()
 
-	if K == 1 {
-		out(0)
+	type E func() int
+	type Merger func(a, b int) int
+	e := func() int {
+		return inf
+	}
+	merger := func(a, b int) int {
+		return min(a, b)
+	}
+	dp := make([]int, N+1)
+	for i := 0; i < N+1; i++ {
+		dp[i] = inf
+	}
+	dp[0] = 1
+	seg := newSegtree(dp, e, merger)
+	for i := 1; i < N+1; i++ {
+		if s[i] == '1' {
+			continue
+		}
+		l := max(0, i-M)
+		r := i
+		m := seg.Prod(l, r)
+		if m == inf {
+			continue
+		}
+		dp[i] = m + 1
+		seg.Set(i, m+1)
+	}
+
+	if dp[N] == inf {
+		out(-1)
 		return
 	}
-	a := getInts(N)
-	b := make([]int, N+1)
-	for i := 0; i < N; i++ {
-		b[i+1] = (b[i] + a[i]) % K
-	}
 
-	ans := 0
-	cnt := make(map[int]int)
-	cnt[0] = 1
-	for r := 1; r < N+1; r++ {
-		// Sr - r % K = x となる要素を追加
-		x := ((b[r]-r)%K + K) % K
-		ans += cnt[x]
-		cnt[x]++
-		// 外れた部分(幅Ｋから外れた部分）を引く
-		if 0 <= r-K+1 {
-			y := ((b[r-K+1]-(r-K+1))%K + K) % K
-			cnt[y]--
+	cur := N
+	// out(dp)
+	ans := []int{}
+	for {
+		next := cur
+		for i := cur; i >= max(0, cur-M); i-- {
+			// out(i)
+			if dp[i] == dp[cur]-1 {
+				next = i
+			}
+		}
+		// out("next=", next)
+		ans = append(ans, cur-next)
+		cur = next
+		if cur == 0 {
+			break
 		}
 	}
-	out(ans)
+
+	for i := len(ans) - 1; i >= 0; i-- {
+		out(ans[i])
+	}
 }

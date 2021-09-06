1561e437ee0231ce5a1b14585c88594399042550
aruaru0 <aruaru0@gmail.com>
2021-09-06 20:59:52 +0900

    20210906

---
+++ ABC/217/D/other/main.go
@@ -0,0 +1,358 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+// 平衡二分探索木
+var (
+	y uint = 88172645463325252
+)
+
+const inf = math.MaxInt64
+
+func xorshift() uint {
+	y ^= y << 7
+	y ^= y >> 9
+	return y
+}
+
+type treapNode struct {
+	value    int
+	priority uint
+	count    int
+	left     *treapNode
+	right    *treapNode
+}
+
+func newTreapNode(v int) *treapNode {
+	return &treapNode{v, xorshift(), 1, nil, nil}
+}
+
+func treapRotateRight(n *treapNode) *treapNode {
+	l := n.left
+	n.left = l.right
+	l.right = n
+	return l
+}
+
+func treapRotateLeft(n *treapNode) *treapNode {
+	r := n.right
+	n.right = r.left
+	r.left = n
+	return r
+}
+
+func treapInsert(n *treapNode, v int) *treapNode {
+	if n == nil {
+		return newTreapNode(v)
+	}
+	if n.value == v {
+		n.count++
+		return n
+	}
+	if n.value > v {
+		n.left = treapInsert(n.left, v)
+		if n.priority > n.left.priority {
+			n = treapRotateRight(n)
+		}
+	} else {
+		n.right = treapInsert(n.right, v)
+		if n.priority > n.right.priority {
+			n = treapRotateLeft(n)
+		}
+	}
+	return n
+}
+
+func treapDelete(n *treapNode, v int) *treapNode {
+	if n == nil {
+		panic("node is not found!")
+	}
+	if n.value > v {
+		n.left = treapDelete(n.left, v)
+		return n
+	}
+	if n.value < v {
+		n.right = treapDelete(n.right, v)
+		return n
+	}
+
+	// n.value == v
+	if n.count > 1 {
+		n.count--
+		return n
+	}
+
+	if n.left == nil && n.right == nil {
+		return nil
+	}
+
+	if n.left == nil {
+		n = treapRotateLeft(n)
+	} else if n.right == nil {
+		n = treapRotateRight(n)
+	} else {
+		// n.left != nil && n.right != nil
+		if n.left.priority < n.right.priority {
+			n = treapRotateRight(n)
+		} else {
+			n = treapRotateLeft(n)
+		}
+	}
+	return treapDelete(n, v)
+}
+
+func treapCount(n *treapNode) int {
+	if n == nil {
+		return 0
+	}
+	return n.count + treapCount(n.left) + treapCount(n.right)
+}
+
+func treapString(n *treapNode) string {
+	if n == nil {
+		return ""
+	}
+	result := make([]string, 0)
+	if n.left != nil {
+		result = append(result, treapString(n.left))
+	}
+	result = append(result, fmt.Sprintf("%d:%d", n.value, n.count))
+	if n.right != nil {
+		result = append(result, treapString(n.right))
+	}
+	return strings.Join(result, " ")
+}
+
+func treapMin(n *treapNode) int {
+	if n.left != nil {
+		return treapMin(n.left)
+	}
+	return n.value
+}
+
+func treapLEMax(n *treapNode, v int) int {
+	ret := -inf
+	t := n
+	for t != nil {
+		if t.value <= v {
+			ret = max(ret, t.value)
+		}
+		if t.value > v {
+			t = t.left
+		} else {
+			t = t.right
+		}
+	}
+
+	return ret
+}
+
+func treapGEMin(n *treapNode, v int) int {
+	ret := inf
+	t := n
+	for t != nil {
+		if t.value >= v {
+			ret = min(ret, t.value)
+		}
+		if t.value < v {
+			t = t.right
+		} else {
+			t = t.left
+		}
+	}
+
+	return ret
+}
+
+func treapMax(n *treapNode) int {
+	if n.right != nil {
+		return treapMax(n.right)
+	}
+	return n.value
+}
+
+type treap struct {
+	root *treapNode
+	size int
+}
+
+func (t *treap) Insert(v int) {
+	t.root = treapInsert(t.root, v)
+	t.size++
+}
+
+func (t *treap) Delete(v int) {
+	t.root = treapDelete(t.root, v)
+	t.size--
+}
+
+func (t *treap) String() string {
+	return treapString(t.root)
+}
+
+func (t *treap) Count() int {
+	return t.size
+}
+
+func (t *treap) Min() int {
+	return treapMin(t.root)
+}
+
+func (t *treap) LEMax(v int) int {
+	return treapLEMax(t.root, v)
+}
+
+func (t *treap) Max() int {
+	return treapMax(t.root)
+}
+
+func (t *treap) GEMin(v int) int {
+	return treapGEMin(t.root, v)
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	L, Q := getI(), getI()
+
+	t := &treap{}
+	t.Insert(0)
+	t.Insert(L)
+	for i := 0; i < Q; i++ {
+		c, x := getI(), getI()
+		if c == 1 {
+			t.Insert(x)
+		} else {
+			r := t.GEMin(x)
+			l := t.LEMax(x)
+			out(r - l)
+		}
+	}
+
+}

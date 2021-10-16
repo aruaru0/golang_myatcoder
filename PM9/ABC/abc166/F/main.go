3b5801628a27582112fd81b90208bd7edae97f3e
aruaru0 <aruaru0@gmail.com>
2021-10-16 21:03:33 +0900

    211016

---
+++ PM9/ABC/abc166/F/main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, A, B, C := getI(), getI(), getI(), getI()
+	s := make([]string, N)
+	for i := 0; i < N; i++ {
+		s[i] = getS()
+	}
+
+	f := func(i, n, m int, s []string, t string) (int, int, int, bool) {
+		if n == 0 && m == 0 {
+			return 0, 0, 0, false
+		}
+		sel := 0
+		if n == 0 {
+			n++
+			m--
+			sel = 0
+		} else if m == 0 {
+			n--
+			m++
+			sel = 1
+		} else if i != N-1 && strings.Contains(s[i+1], t) {
+			n++
+			m--
+			sel = 0
+		} else {
+			n--
+			m++
+			sel = 1
+		}
+		return sel, n, m, true
+	}
+
+	ans := []string{}
+	ok := false
+	for i := 0; i < N; i++ {
+		sel := 0
+		switch s[i] {
+		case "AB":
+			sel, A, B, ok = f(i, A, B, s, "A")
+		case "AC":
+			sel, A, C, ok = f(i, A, C, s, "A")
+		case "BC":
+			sel, B, C, ok = f(i, B, C, s, "B")
+		}
+		if !ok {
+			out("No")
+			return
+		}
+		ans = append(ans, (string(s[i][sel])))
+	}
+
+	out("Yes")
+	for _, e := range ans {
+		out(e)
+	}
+}

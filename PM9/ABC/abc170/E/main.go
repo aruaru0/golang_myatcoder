6e048055b7ea0467da7037fe18368408a10f1168
aruaru0 <aruaru0@gmail.com>
2021-10-21 21:14:54 +0900

    211021

---
+++ PM9/ABC/abc170/E/main.go
@@ -0,0 +1,334 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type E func() int
+type Merger func(a, b int) int
+type Compare func(v int) bool
+type Segtree struct {
+	n      int
+	size   int
+	log    int
+	d      []int
+	e      E
+	merger Merger
+}
+
+func newSegtree(v []int, e E, m Merger) *Segtree {
+	seg := new(Segtree)
+	seg.n = len(v)
+	seg.log = seg.ceilPow2(seg.n)
+	seg.size = 1 << uint(seg.log)
+	seg.d = make([]int, 2*seg.size)
+	seg.e = e
+	seg.merger = m
+	for i, _ := range seg.d {
+		seg.d[i] = seg.e()
+	}
+	for i := 0; i < seg.n; i++ {
+		seg.d[seg.size+i] = v[i]
+	}
+	for i := seg.size - 1; i >= 1; i-- {
+		seg.Update(i)
+	}
+	return seg
+}
+func (seg *Segtree) Update(k int) {
+	seg.d[k] = seg.merger(seg.d[2*k], seg.d[2*k+1])
+}
+func (seg *Segtree) Set(p, x int) {
+	p += seg.size
+	seg.d[p] = x
+	for i := 1; i <= seg.log; i++ {
+		seg.Update(p >> uint(i))
+	}
+}
+func (seg *Segtree) Get(p int) int {
+	return seg.d[p+seg.size]
+}
+func (seg *Segtree) Prod(l, r int) int {
+	sml, smr := seg.e(), seg.e()
+	l += seg.size
+	r += seg.size
+	for l < r {
+		if (l & 1) == 1 {
+			sml = seg.merger(sml, seg.d[l])
+			l++
+		}
+		if (r & 1) == 1 {
+			r--
+			smr = seg.merger(seg.d[r], smr)
+		}
+		l >>= 1
+		r >>= 1
+	}
+	return seg.merger(sml, smr)
+}
+func (seg *Segtree) AllProd() int {
+	return seg.d[1]
+}
+func (seg *Segtree) MaxRight(l int, cmp Compare) int {
+	if l == seg.n {
+		return seg.n
+	}
+	l += seg.size
+	sm := seg.e()
+	for {
+		for l%2 == 0 {
+			l >>= 1
+		}
+		if !cmp(seg.merger(sm, seg.d[l])) {
+			for l < seg.size {
+				l = 2 * l
+				if cmp(seg.merger(sm, seg.d[l])) {
+					sm = seg.merger(sm, seg.d[l])
+					l++
+				}
+			}
+			return l - seg.size
+		}
+		sm = seg.merger(sm, seg.d[l])
+		l++
+		if l&-l == l {
+			break
+		}
+	}
+	return seg.n
+}
+func (seg *Segtree) MinLeft(r int, cmp Compare) int {
+	if r == 0 {
+		return 0
+	}
+	r += seg.size
+	sm := seg.e()
+	for {
+		r--
+		for r > 1 && r%2 != 0 {
+			r >>= 1
+		}
+		if !cmp(seg.merger(seg.d[r], sm)) {
+			for r < seg.size {
+				r = 2*r + 1
+				if cmp(seg.merger(seg.d[r], sm)) {
+					sm = seg.merger(seg.d[r], sm)
+					r--
+				}
+			}
+			return r + 1 - seg.size
+		}
+		sm = seg.merger(seg.d[r], sm)
+		if r&-r == r {
+			break
+		}
+	}
+	return 0
+}
+func (seg *Segtree) ceilPow2(n int) int {
+	x := 0
+	for (1 << uint(x)) < n {
+		x++
+	}
+	return x
+}
+
+type pqi struct{ a, idx int }
+
+type priorityQueue []pqi
+
+func (pq priorityQueue) Len() int            { return len(pq) }
+func (pq priorityQueue) Swap(i, j int)       { pq[i], pq[j] = pq[j], pq[i] }
+func (pq priorityQueue) Less(i, j int) bool  { return pq[i].a > pq[j].a }
+func (pq *priorityQueue) Push(x interface{}) { *pq = append(*pq, x.(pqi)) }
+func (pq *priorityQueue) Pop() interface{} {
+	x := (*pq)[len(*pq)-1]
+	*pq = (*pq)[0 : len(*pq)-1]
+	return x
+}
+
+const inf = int(1e18)
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, Q := getI(), getI()
+	a := make([]int, N) // レート
+	b := make([]int, N) // 現在の場所
+	for i := 0; i < N; i++ {
+		a[i], b[i] = getI(), getI()-1
+	}
+
+	pq := make([]priorityQueue, 210000) // 各園の優先キュー
+	for i := 0; i < N; i++ {
+		heap.Push(&pq[b[i]], pqi{a[i], i}) // 初期位置に配置
+	}
+
+	v := make([]int, 210000)
+	for i := 0; i < 210000; i++ {
+		if len(pq[i]) == 0 {
+			v[i] = inf // 園児がいなければinf
+		} else {
+			v[i] = pq[i][0].a // 園児がいれば最大のレートを代入
+		}
+	}
+	e := func() int {
+		return inf
+	}
+	merger := func(a, b int) int {
+		return min(a, b)
+	}
+
+	seg := newSegtree(v, e, merger) // セグメントツリー（min）
+
+	for i := 0; i < Q; i++ {
+		c, d := getI()-1, getI()-1 // 園児CをDに移動させる
+		pos := b[c]                // 現在の園児の位置
+		b[c] = d                   // 園児を移動させる
+
+		// 移動元の処理
+		for b[pq[pos][0].idx] != pos { // 現在位置のレート最大の園児が既に移動していれば
+			heap.Pop(&pq[pos]) // キューから削除
+			if len(pq[pos]) == 0 {
+				break
+			}
+		}
+		if len(pq[pos]) == 0 {
+			seg.Set(pos, inf)
+		} else {
+			seg.Set(pos, pq[pos][0].a)
+		}
+
+		// 移動先の処理
+		heap.Push(&pq[d], pqi{a[c], c})
+		for b[pq[d][0].idx] != d {
+			heap.Pop(&pq[d])
+		}
+		seg.Set(d, pq[d][0].a)
+		out(seg.Prod(0, 210000))
+	}
+}

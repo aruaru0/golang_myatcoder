1b33927bca4e6d9fa80b966f1d00abbb352bec06
aruaru0 <aruaru0@gmail.com>
2021-11-07 21:38:39 +0900

    211107

---
+++ PM9/ABC/abc178/F/main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+	a := getInts(N)
+	b := getInts(N)
+	A := make([]int, N+1)
+	B := make([]int, N+1)
+	// 各数字の出現数をカウント
+	for i := 0; i < N; i++ {
+		A[a[i]]++
+		B[b[i]]++
+	}
+	// AとBの同じ数字の数を足してＮより大きい場合はNO
+	for i := 0; i < N+1; i++ {
+		if A[i]+B[i] > N {
+			out("No")
+			return
+		}
+	}
+
+	// 累積和を作る
+	for i := 1; i <= N; i++ {
+		A[i] += A[i-1]
+		B[i] += B[i-1]
+	}
+
+	// ずらす量の最大値を求める
+	x := 0
+	for i := 1; i <= N; i++ {
+		x = max(x, A[i]-B[i-1])
+	}
+	out("Yes")
+	for i := 0; i < N; i++ {
+		fmt.Fprint(wr, b[(i+N-x)%N], " ")
+	}
+	out()
+}

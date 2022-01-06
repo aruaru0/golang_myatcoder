2d978e1f669bac8ba6393ed01e7d2f8c9263c7d0
aruaru0 <aruaru0@gmail.com>
2022-01-06 21:57:01 +0900

    PAST06-K

---
+++ ETC/PAST006/K/main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+const inf = int(1000)
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+	P := make([]int, N)
+	U := make([]int, N)
+	for i := 0; i < N; i++ {
+		P[i], U[i] = getI(), getI()
+	}
+
+	dp := make([][100]int, N+1)
+	for j := 0; j <= N; j++ {
+		for i := 0; i < 100; i++ {
+			dp[j][i] = -inf
+		}
+	}
+	dp[0][0] = 0
+
+	for i := 0; i < N; i++ {
+		div := P[i] / 100
+		mod := P[i] % 100
+		// out(div, mod, P[i], U[i]-P[i])
+		for j := 0; j < 100; j++ {
+			dp[i+1][j] = max(dp[i+1][j], dp[i][j])
+			plus := 0
+			if j+mod >= 100 {
+				plus = 20
+			}
+			dp[i+1][(j+mod)%100] = max(dp[i][(j+mod)%100], dp[i][j]+U[i]-P[i]+plus+div*20)
+		}
+		// out(dp[i+1])
+	}
+	ans := 0
+	for i := 0; i < 100; i++ {
+		ans = max(ans, dp[N][i])
+	}
+	out(ans)
+}

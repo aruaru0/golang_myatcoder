be9b510ac4decb465f09743ebde74a68fe6d1386
aruaru0 <aruaru0@gmail.com>
2022-01-11 22:32:00 +0900

    abc200-e

---
+++ PM9/ABC/abc200/E/main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, K := getI(), getI()
+	dp := make([][3000005]int, 4)
+	dp[0][0] = 1
+	for i := 0; i < 3; i++ {
+		for j := 0; j <= i*N; j++ {
+			dp[i+1][j+1] += dp[i][j]
+			dp[i+1][j+N+1] -= dp[i][j]
+		}
+		for j := 1; j <= (i+1)*N; j++ {
+			dp[i+1][j] += dp[i+1][j-1]
+		}
+	}
+
+	x := 0
+	for i := 3; i <= 3*N; i++ {
+		if K <= dp[3][i] {
+			x = i
+			break
+		}
+		K -= dp[3][i]
+	}
+
+	for i := 1; i <= N; i++ {
+		jmi := max(1, x-i-N)
+		jma := min(N, x-i-1)
+		if jmi > jma {
+			continue
+		}
+		if K > (jma - jmi + 1) {
+			K -= jma - jmi + 1
+			continue
+		}
+		y := jmi + K - 1
+		z := x - i - y
+		out(i, y, z)
+		return
+	}
+}

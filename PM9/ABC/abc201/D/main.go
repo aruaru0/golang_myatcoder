034f288385ff6ac8a74074156139c9673b91574d
aruaru0 <aruaru0@gmail.com>
2022-01-15 22:17:57 +0900

    abc201-CD

---
+++ PM9/ABC/abc201/D/main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+var H, W int
+var a []string
+
+type pos struct {
+	x, y int
+}
+
+var dx = []int{0, 1}
+var dy = []int{1, 0}
+
+const inf = int(1e15)
+
+var memo [2100][2100]*int
+
+func rec(x, y, turn int) int {
+	// 最終コマについたら終了
+	if x == W-1 && y == H-1 {
+		return 0
+	}
+	// メモ
+	if memo[y][x] != nil {
+		return *memo[y][x]
+	}
+	ret := -inf
+	// 右または下に移動させる
+	for i := 0; i < 2; i++ {
+		px := x + dx[i]
+		py := y + dy[i]
+		if px >= W || py >= H {
+			continue
+		}
+		//　駒が+なら加算、-なら減算
+		pt := 1
+		if a[py][px] == '-' {
+			pt = -1
+		}
+		// 再帰
+		r := rec(px, py, turn^1)
+		// ポイントは、現在の獲得点数-移動先の獲得点数
+		// 交互に実行するので、-(相手-(自分-(相手... となり
+		//  -相手+自分-相手...となる
+		ret = max(ret, pt-r)
+	}
+	// メモ
+	memo[y][x] = &ret
+	return ret
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	H, W = getI(), getI()
+	a = make([]string, H)
+	for i := 0; i < H; i++ {
+		a[i] = getS()
+	}
+
+	ret := rec(0, 0, 0)
+	if ret > 0 {
+		out("Takahashi ")
+		return
+	}
+	if ret == 0 {
+		out("Draw")
+		return
+	}
+	out("Aoki")
+}

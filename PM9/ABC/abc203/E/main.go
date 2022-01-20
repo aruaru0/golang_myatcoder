f6f97f037b7ec4dfb35fe377df48f5bb6c65f4d5
aruaru0 <aruaru0@gmail.com>
2022-01-20 21:48:54 +0900

    abc203-E

---
+++ PM9/ABC/abc203/E/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type pair struct {
+	x, y int
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, M := getI(), getI()
+	p := make([]pair, M)
+	for i := 0; i < M; i++ {
+		p[i] = pair{getI(), getI()}
+	}
+	sort.Slice(p, func(i, j int) bool {
+		return p[i].x < p[j].x
+	})
+	// out("p", p)
+	m := make(map[int]bool)
+	m[N] = true
+	idx := 0
+	for idx < M {
+		set := make([]int, 0)
+		reset := make([]int, 0)
+		x := p[idx].x
+		// out("-----", m)
+		for idx < M && p[idx].x == x {
+			// out("p = ", p[idx], m[p[idx].y-1], m[p[idx].y+1])
+			if m[p[idx].y-1] || m[p[idx].y+1] {
+				set = append(set, p[idx].y)
+			} else {
+				reset = append(reset, p[idx].y)
+			}
+			idx++
+		}
+		for _, e := range set {
+			m[e] = true
+		}
+		for _, e := range reset {
+			delete(m, e)
+		}
+		// out(set, reset, m)
+	}
+	out(len(m))
+}

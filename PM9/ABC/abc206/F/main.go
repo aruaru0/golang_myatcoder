339b43ff29a9d284773f17f0f22a7c3d57d0db42
aruaru0 <aruaru0@gmail.com>
2022-01-28 21:57:07 +0900

    abc206-F

---
+++ PM9/ABC/abc206/F/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+var N int
+var L, R []int
+var memo [][110]*int
+
+func grundy(l, r int) int {
+	if memo[l][r] != nil {
+		return *memo[l][r]
+	}
+	gs := make(map[int]bool)
+	for i := 0; i < N; i++ {
+		// 区間が[l, r)に入っている場合は、分割していく
+		if l <= L[i] && R[i] <= r {
+			x := grundy(l, L[i]) ^ grundy(R[i], r)
+			gs[x] = true
+		}
+	}
+
+	g := 0
+	for gs[g] {
+		g++
+	}
+	memo[l][r] = &g
+	return g
+}
+
+func solve() {
+	N = getI()
+	L = make([]int, N)
+	R = make([]int, N)
+	for i := 0; i < N; i++ {
+		L[i] = getI()
+		R[i] = getI()
+	}
+	memo = make([][110]*int, 110)
+	if grundy(1, 100) == 0 {
+		out("Bob")
+		return
+	}
+	out("Alice")
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	T := getI()
+	for i := 0; i < T; i++ {
+		solve()
+	}
+}

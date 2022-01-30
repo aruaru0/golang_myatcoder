944e6ffaae992ea1b0a5ec24fcc081d5914a87b2
aruaru0 <aruaru0@gmail.com>
2022-01-30 21:35:45 +0900

    abc207-E

---
+++ PM9/ABC/abc207/E/main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+const mod = int(1e9 + 7)
+
+// 
+// これ、いまいち理解できない。
+// 
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+	A := getInts(N)
+
+	// dp[i][k] := i番目まででk組作った
+	var dp [3100][3100]int
+	// tot[k][mo] := k組作っていてこれまでの総和を(k+1)で割った余りがmoである
+	var tot [3100][3100]int
+
+	dp[0][0] = 1
+	tot[0][0] = 1
+	sum := 0
+	for i := 0; i < N; i++ {
+		sum += A[i]
+		for k := 1; k <= N; k++ {
+			dp[i+1][k] = tot[k-1][sum%k]
+		}
+		for k := 1; k <= N; k++ {
+			tot[k][sum%(k+1)] += dp[i+1][k]
+			tot[k][sum%(k+1)] %= mod
+		}
+	}
+
+	ans := 0
+	for k := 1; k <= N; k++ {
+		ans += dp[N][k]
+		ans %= mod
+	}
+	out(ans)
+}

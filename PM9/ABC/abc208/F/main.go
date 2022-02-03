f5d8ee296fb633e90ef59ffaa9a9cbb0bc8099d0
aruaru0 <aruaru0@gmail.com>
2022-02-03 21:13:05 +0900

    abc208-F

---
+++ PM9/ABC/abc208/F/main.go
@@ -0,0 +1,210 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	// 手持ちのmodintライブラリではTLEした。厳しい・・・
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	n, m, k := getI(), getI(), getI()
+	d := k + m
+	b := make([]int, d+1)
+	s := make([]int, d+2)
+	s[d+1] = 1
+
+	for i := 0; i < d+1; i++ {
+		b[i] = powMod(i, k)
+	}
+	for j := 0; j < m; j++ {
+		for i := 0; i < d; i++ {
+			b[i+1] += b[i]
+			b[i+1] %= mod
+		}
+	}
+
+	x := n % mod
+	for i := d; i+1 > 0; i-- {
+		s[i] = s[i+1] * (x - i + mod) % mod
+	}
+
+	t := 1
+	for i := 1; i < d+1; i++ {
+		t *= (-i + mod) % mod
+		t %= mod
+	}
+	t = invMod(t)
+
+	y := 0
+	for i := 1; i < d+2; i++ {
+		y += t * s[i] % mod * b[i-1] % mod
+		y %= mod
+		t *= divMod(((x-i+1)*(-(d-i+1)+mod)%mod)%mod, i)
+		t %= mod
+	}
+	fmt.Println(y)
+}
+
+const mod = 1000000007
+
+func powMod(a, n int) int {
+	res := 1
+	for n > 0 {
+		if n%2 == 1 {
+			res = res * a % mod
+		}
+		a = a * a % mod
+		n /= 2
+	}
+	return res
+}
+
+func invMod(a int) int {
+	return powMod(a, mod-2)
+}
+
+func divMod(a, b int) int {
+	ret := a * modInv(b)
+	ret %= mod
+	return ret
+}
+
+func modInv(a int) int {
+	b, u, v := mod, 1, 0
+	for b != 0 {
+		t := a / b
+		a -= t * b
+		a, b = b, a
+		u -= t * v
+		u, v = v, u
+	}
+	u %= mod
+	if u < 0 {
+		u += mod
+	}
+	return u
+}

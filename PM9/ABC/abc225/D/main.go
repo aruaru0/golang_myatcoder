84b749f757d77d4ffb08d1a7839fb6df79cdc153
aruaru0 <aruaru0@gmail.com>
2022-04-07 21:33:01 +0900

    abc225-D

---
+++ PM9/ABC/abc225/D/main.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type Que struct {
+	prev, next int
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, Q := getI(), getI()
+
+	que := make([]Que, N)
+	for i := 0; i < N; i++ {
+		que[i].prev = -1
+		que[i].next = -1
+	}
+	for i := 0; i < Q; i++ {
+		t := getI()
+		if t == 1 {
+			x, y := getI()-1, getI()-1
+			que[x].next = y
+			que[y].prev = x
+		} else if t == 2 {
+			x, y := getI()-1, getI()-1
+			que[x].next = -1
+			que[y].prev = -1
+		} else {
+			x := getI() - 1
+			for que[x].prev != -1 {
+				x = que[x].prev
+			}
+			ans := make([]int, 0)
+			for que[x].next != -1 {
+				ans = append(ans, x)
+				x = que[x].next
+			}
+			ans = append(ans, x)
+			fmt.Fprint(wr, len(ans))
+			for _, e := range ans {
+				fmt.Fprint(wr, " ", e+1)
+			}
+			out()
+		}
+	}
+
+}

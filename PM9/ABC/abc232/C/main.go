eddf453d97983e0801aad199f7336136826bdb8a
aruaru0 <aruaru0@gmail.com>
2022-04-30 22:13:13 +0900

    abc232-C

---
+++ PM9/ABC/abc232/C/main.go
@@ -0,0 +1,204 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func NextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, M := getI(), getI()
+
+	nodeA := make([]map[int]bool, N)
+	nodeB := make([]map[int]bool, N)
+	for i := 0; i < N; i++ {
+		nodeA[i] = make(map[int]bool)
+		nodeB[i] = make(map[int]bool)
+	}
+	for i := 0; i < M; i++ {
+		a, b := getI()-1, getI()-1
+		nodeA[a][b] = true
+		nodeA[b][a] = true
+	}
+	for i := 0; i < M; i++ {
+		c, d := getI()-1, getI()-1
+		nodeB[c][d] = true
+		nodeB[d][c] = true
+	}
+
+	a := make([]int, N)
+	for i := 0; i < N; i++ {
+		a[i] = i
+	}
+
+	for {
+		ok := true
+		for i := 0; i < N; i++ {
+			if len(nodeA[i]) != len(nodeB[a[i]]) {
+				ok = false
+				break
+			}
+			for e := range nodeA[i] {
+				if !nodeB[a[i]][a[e]] {
+					ok = false
+				}
+			}
+		}
+		if ok {
+			out("Yes")
+			return
+		}
+
+		if !NextPermutation(sort.IntSlice(a)) {
+			break
+		}
+	}
+	out("No")
+}

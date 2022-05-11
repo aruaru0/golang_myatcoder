1797ad9c666a27a2cdd30c1c400925477a7a5d05
aruaru0 <aruaru0@gmail.com>
2022-05-11 21:29:12 +0900

    abc235-D

---
+++ PM9/ABC/abc235/D/main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+const inf = int(1e18)
+const mx = 1000000
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	a, N := getI(), getI()
+
+	dist := make([]int, mx)
+	for i := 0; i < mx; i++ {
+		dist[i] = inf
+	}
+	q := make([]int, 0)
+
+	push := func(v, d int) {
+		if dist[v] != inf {
+			return
+		}
+		dist[v] = d
+		q = append(q, v)
+	}
+
+	push(1, 0)
+	for len(q) != 0 {
+		v := q[0]
+		q = q[1:]
+		d := dist[v]
+		if v*a < mx {
+			push(v*a, d+1)
+		}
+		if v >= 10 && v%10 != 0 {
+			s := strconv.FormatInt(int64(v), 10)
+			l := len(s)
+			if l > 1 {
+				s = s[l-1:] + s[:l-1]
+			}
+			u, _ := strconv.Atoi(s)
+			push(u, d+1)
+		}
+	}
+
+	if dist[N] == inf {
+		out(-1)
+		return
+	}
+	out(dist[N])
+}

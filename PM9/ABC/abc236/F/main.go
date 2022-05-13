d145fb7fa5fbdfda4419ecbd63b6fcdd52f14af3
aruaru0 <aruaru0@gmail.com>
2022-05-13 21:54:08 +0900

    abc236-F

---
+++ PM9/ABC/abc236/F/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type pair struct {
+	c, p int
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+	n := 1<<N - 1
+	p := make([]pair, n)
+	for i := 0; i < n; i++ {
+		p[i] = pair{getI(), i + 1}
+	}
+
+	// 順列をcが小さい順に並べる
+	sort.Slice(p, func(i, j int) bool {
+		return p[i].c < p[j].c
+	})
+
+	// 集合Sを初期化
+	ans := 0
+	s := make([]bool, n+1)
+	for i := 0; i < n; i++ {
+		// pがすでに作れている場合はスキップ
+		if s[p[i].p] == true {
+			continue
+		}
+		// 作れていない場合は、pを用いて作れる値を更新する
+		t := make([]bool, n+1)
+		t[p[i].p] = true
+		for j := 1; j <= n; j++ {
+			if s[j] == true {
+				t[j] = true
+				x := j ^ p[i].p
+				t[x] = true
+			}
+		}
+		// 答えにコストCを加算
+		ans += p[i].c
+		s, t = t, s
+	}
+	out(ans)
+}

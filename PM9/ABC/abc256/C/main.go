011bc7bf2351c668a5f49a56c5b99b767d1a509e
aruaru0 <55325119+aruaru0@users.noreply.github.com>
2022-07-09 12:51:53 +0000

    abc256-cd

---
+++ PM9/ABC/abc256/C/main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func check(a, b, c, d int, H, W []int) bool {
+	e := H[0] - a - b
+	f := H[1] - c - d
+	g := W[0] - a - c
+	h := W[1] - b - d
+
+	if e <= 0 || f <= 0 || g <= 0 || h <= 0 {
+		return false
+	}
+
+	i0 := H[2] - g - h
+	i1 := W[2] - e - f
+	if i0 <= 0 || i1 <= 0 {
+		return false
+	}
+	if i0 != i1 {
+		return false
+	}
+	return true
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	h := getInts(3)
+	w := getInts(3)
+
+	cnt := 0
+	for i := 1; i < 30; i++ {
+		for j := 1; j < 30; j++ {
+			for k := 1; k < 30; k++ {
+				for l := 1; l < 30; l++ {
+					if check(i, j, k, l, h, w) {
+						cnt++
+					}
+				}
+			}
+		}
+	}
+	out(cnt)
+}

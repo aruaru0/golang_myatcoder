d0d257709102c0ab9a0871ab162ba6178d218576
aruaru0 <55325119+aruaru0@users.noreply.github.com>
2022-07-15 13:04:41 +0000

    abc258-B

---
+++ PM9/ABC/abc258/B/main.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+var dx = []int{-1, -1, -1, 0, 0, 1, 1, 1}
+var dy = []int{-1, 0, 1, -1, 1, -1, 0, 1}
+
+func check(x, y, k int) int {
+	ret := a[x][y]
+	for i := 0; i < N-1; i++ {
+		x = x + dx[k]
+		y = y + dy[k]
+		if x < 0 {
+			x += N
+		}
+		if y < 0 {
+			y += N
+		}
+		if x >= N {
+			x %= N
+		}
+		if y >= N {
+			y %= N
+		}
+		ret = ret*10 + a[x][y]
+	}
+	return ret
+}
+
+var N int
+var a [][]int
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N = getI()
+	s := make([]string, N)
+	for i := 0; i < N; i++ {
+		s[i] = getS()
+	}
+	a = make([][]int, N)
+	for i := 0; i < N; i++ {
+		a[i] = make([]int, N)
+		for j := 0; j < N; j++ {
+			a[i][j] = int(s[i][j] - '0')
+		}
+	}
+
+	ans := 0
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			for k := 0; k < 8; k++ {
+				ans = max(ans, check(i, j, k))
+			}
+		}
+	}
+	out(ans)
+}

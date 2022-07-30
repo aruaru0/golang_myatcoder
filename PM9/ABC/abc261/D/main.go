3d4974dad9264b50168d9adcc075bb40c1d9b31e
aruaru0 <aruaru0@gmail.com>
2022-07-30 21:39:21 +0900

    abc261-D

---
+++ PM9/ABC/abc261/D/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+var x []int
+var y []int
+var N, M int
+
+const inf int = 1e18
+
+var memo [][]int
+
+func rec(c, n int) int {
+	if n == N {
+		return 0
+	}
+	if memo[c][n] != -inf {
+		return memo[c][n]
+	}
+	// ura
+	ret0 := rec(0, n+1)
+	// omote
+	ret1 := rec(c+1, n+1) + x[n] + y[c+1]
+	ret := max(ret0, ret1)
+	memo[c][n] = ret
+	return ret
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, M = getI(), getI()
+	x = getInts(N)
+	y = make([]int, N+1)
+	for i := 0; i < M; i++ {
+		C, Y := getI(), getI()
+		y[C] = Y
+	}
+	memo = make([][]int, N+1)
+	for i := 0; i < N+1; i++ {
+		memo[i] = make([]int, N+1)
+		for j := 0; j < N+1; j++ {
+			memo[i][j] = -inf
+		}
+	}
+	out(rec(0, 0))
+}

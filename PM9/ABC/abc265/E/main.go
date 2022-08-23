05578749ba3300da3c7fd5343276ce1df865a4d0
aruaru0 <aruaru0@gmail.com>
2022-08-23 21:28:44 +0900

    abc265-E

---
+++ PM9/ABC/abc265/E/main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type pair struct {
+	x, y int
+}
+
+const mod = 998244353
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, M := getI(), getI()
+	dxdy := make(map[pair]bool)
+	for i := 0; i < 3; i++ {
+		dxdy[pair{getI(), getI()}] = true
+	}
+
+	p := make(map[pair]bool, M)
+	for i := 0; i < M; i++ {
+		p[pair{getI(), getI()}] = true
+	}
+	dp := make(map[pair]int)
+	dp[pair{0, 0}] = 1
+	for i := 0; i < N; i++ {
+		tmp := make(map[pair]int)
+		for e := range dp {
+			x, y, val := e.x, e.y, dp[e]
+			for v := range dxdy {
+				dx, dy := v.x, v.y
+				if p[pair{x + dx, y + dy}] == false {
+					tmp[pair{x + dx, y + dy}] += val
+					tmp[pair{x + dx, y + dy}] %= mod
+				}
+			}
+		}
+		dp, tmp = tmp, dp
+	}
+	ans := 0
+	for e := range dp {
+		ans += dp[e]
+		ans %= mod
+	}
+	out(ans)
+}

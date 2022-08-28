58121f9784ee5717ff4deffe9c507b3ae5cc746d
aruaru0 <aruaru0@gmail.com>
2022-08-28 21:11:36 +0900

    abc266-C

---
+++ PM9/ABC/abc266/C/main.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"image"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+// ★ 注意：すべての点が同一直線にある時は、バグります！！★
+// ConvexHull returns the set of points that define the
+// convex hull of p in CCW order starting from the left most.
+func (p points) ConvexHull() points {
+	// From https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain
+	// with only minor deviations.
+	sort.Sort(p)
+	var h points
+
+	// Lower hull
+	for _, pt := range p {
+		for len(h) >= 2 && !ccw(h[len(h)-2], h[len(h)-1], pt) {
+			h = h[:len(h)-1]
+		}
+		h = append(h, pt)
+	}
+
+	// Upper hull
+	for i, t := len(p)-2, len(h)+1; i >= 0; i-- {
+		pt := p[i]
+		for len(h) >= t && !ccw(h[len(h)-2], h[len(h)-1], pt) {
+			h = h[:len(h)-1]
+		}
+		h = append(h, pt)
+	}
+
+	return h[:len(h)-1]
+}
+
+// ccw returns true if the three points make a counter-clockwise turn
+func ccw(a, b, c image.Point) bool {
+	return ((b.X - a.X) * (c.Y - a.Y)) > ((b.Y - a.Y) * (c.X - a.X))
+}
+
+type points []image.Point
+
+func (p points) Len() int      { return len(p) }
+func (p points) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+func (p points) Less(i, j int) bool {
+	if p[i].X == p[j].X {
+		return p[i].Y < p[i].Y
+	}
+	return p[i].X < p[j].X
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	pts := points{
+		{getI(), getI()},
+		{getI(), getI()},
+		{getI(), getI()},
+		{getI(), getI()},
+	}
+
+	hull := pts.ConvexHull()
+
+	if len(hull) == 4 {
+		out("Yes")
+	} else {
+		out("No")
+	}
+}

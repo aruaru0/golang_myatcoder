3eb04a7b07709ee3c0022ae380ee36008f7c35e6
aruaru0 <aruaru0@gmail.com>
2022-09-04 21:44:23 +0900

    abc267abc

---
+++ PM9/ABC/abc267/B/main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	s := getS()
+	N := 7
+
+	x := make([]int, 7)
+	p := []int{3, 2, 4, 1, 3, 5, 0, 2, 4, 6}
+	for i, e := range s {
+		if e == '1' {
+			x[p[i]]++
+		}
+	}
+
+	l, r := 0, 0
+	for i := 0; i < N; i++ {
+		if x[i] != 0 {
+			l = i
+			break
+		}
+	}
+	for i := N - 1; i >= 0; i-- {
+		if x[i] != 0 {
+			r = i
+			break
+		}
+	}
+
+	if l == r {
+		out("No")
+		return
+	}
+
+	ok := false
+	for i := l; i <= r; i++ {
+		if x[i] == 0 {
+			ok = true
+		}
+	}
+
+	if ok && s[0] == '0' {
+		out("Yes")
+	} else {
+		out("No")
+	}
+}

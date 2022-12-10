2ef79c5b51d3ae10670b05030ac8099a7e8b24b7
aruaru0 <aruaru0@gmail.com>
2022-12-11 04:37:45 +0900

    abc281-E

--- ABC/281/E/main.go
+++ ABC/281/E/main.go
@@ -3,20 +3,230 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
-	"sort"
 	"strconv"
+	"strings"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
-var wr = bufio.NewWriter(os.Stdout)
 
-func out(x ...interface{}) {
-	fmt.Fprintln(wr, x...)
+func main() {
+	sc.Buffer(make([]byte, 1000), 500000)
+	sc.Split(bufio.ScanWords)
+	n, m, k := nextInt(), nextInt(), nextInt()
+	a := make([]int, n)
+	for i := range a {
+		a[i] = nextInt()
+	}
+	bt := NewBinaryTrie(32)
+	for i := 0; i < m; i++ {
+		bt.Add(a[i], 1)
+	}
+	sumk := 0
+	for i := 0; i < k; i++ {
+		sumk += bt.GetSmallKth(i + 1)
+	}
+	ans := make([]int, n-m+1)
+	ans[0] = sumk
+	for i := 1; i <= n-m; i++ {
+		idx := bt.LowerBound(a[i-1])
+		if idx < k {
+			sumk -= a[i-1]
+			sumk += bt.GetSmallKth(k + 1)
+		}
+		bt.Sub(a[i-1], 1)
+		bt.Add(a[i+m-1], 1)
+		idx = bt.LowerBound(a[i+m-1])
+		if idx < k {
+			sumk += a[i+m-1]
+			sumk -= bt.GetSmallKth(k + 1)
+		}
+		ans[i] = sumk
+	}
+	ansstr := make([]string, len(ans))
+	for i := range ans {
+		ansstr[i] = strconv.Itoa(ans[i])
+	}
+	fmt.Println(strings.Join(ansstr, " "))
 }
 
-func getI() int {
+type BinaryTrie struct {
+	root    *BinaryTrieNode
+	bitsize int
+}
+
+type BinaryTrieNode struct {
+	cnt         int
+	left, right *BinaryTrieNode
+}
+
+func NewBinaryTrie(bitsize int) *BinaryTrie {
+	return &BinaryTrie{
+		root:    NewBinaryTrieNode(),
+		bitsize: bitsize,
+	}
+}
+
+func NewBinaryTrieNode() *BinaryTrieNode {
+	return &BinaryTrieNode{}
+}
+
+func (t *BinaryTrie) Add(x, num int) {
+	tmp := t.root
+	for i := t.bitsize - 1; i >= 0; i-- {
+		tmp.cnt += num
+		if (x>>i)&1 == 0 {
+			if tmp.left == nil {
+				tmp.left = NewBinaryTrieNode()
+			}
+			tmp = tmp.left
+		} else {
+			if tmp.right == nil {
+				tmp.right = NewBinaryTrieNode()
+			}
+			tmp = tmp.right
+		}
+	}
+	tmp.cnt += num
+}
+
+func (t *BinaryTrie) Sub(x, num int) {
+	t.Add(x, -num)
+}
+
+func (t *BinaryTrie) Set(x, num int) {
+	tmp := t.Find(x)
+	t.Add(x, num-tmp)
+}
+
+func (t *BinaryTrie) Remove(x int) {
+	t.Set(x, 0)
+}
+
+func (t *BinaryTrie) Find(x int) int {
+	tmp := t.root
+	for i := t.bitsize - 1; i >= 0; i-- {
+		if (x>>i)&1 == 0 {
+			if tmp.left == nil {
+				return 0
+			}
+			tmp = tmp.left
+		} else {
+			if tmp.right == nil {
+				return 0
+			}
+			tmp = tmp.right
+		}
+	}
+	return tmp.cnt
+}
+
+func (t *BinaryTrie) GetMax() int {
+	tmp := t.root
+	if t.root.cnt == 0 {
+		return -1
+	}
+	result := 0
+	for i := 0; i < t.bitsize; i++ {
+		result <<= 1
+		if tmp.right == nil || tmp.right.cnt == 0 {
+			tmp = tmp.left
+		} else {
+			tmp = tmp.right
+			result += 1
+		}
+	}
+	return result
+}
+
+func (t *BinaryTrie) GetMin() int {
+	tmp := t.root
+	if t.root.cnt == 0 {
+		return -1
+	}
+	result := 0
+	for i := 0; i < t.bitsize; i++ {
+		result <<= 1
+		if tmp.left == nil || tmp.left.cnt == 0 {
+			tmp = tmp.right
+			result += 1
+		} else {
+			tmp = tmp.left
+		}
+	}
+	return result
+}
+
+func (t *BinaryTrie) GetSmallKth(k int) int {
+	tmp := t.root
+	if k > t.root.cnt {
+		return -1
+	}
+	pos := k
+	result := 0
+	for i := 0; i < t.bitsize; i++ {
+		result <<= 1
+		if tmp.left == nil {
+			tmp = tmp.right
+			result += 1
+		} else if tmp.left.cnt < pos {
+			pos -= tmp.left.cnt
+			tmp = tmp.right
+			result += 1
+		} else {
+			tmp = tmp.left
+		}
+	}
+	return result
+}
+
+func (t *BinaryTrie) GetBigKth(k int) int {
+	if k > t.root.cnt {
+		return -1
+	}
+	sKth := t.root.cnt - k + 1
+	return t.GetSmallKth(sKth)
+}
+
+func (t *BinaryTrie) LowerBound(x int) int {
+	tmp := t.root
+	result := 0
+	for i := t.bitsize - 1; i >= 0; i-- {
+		if (x>>i)&1 == 0 {
+			if tmp.left == nil {
+				return result
+			}
+			tmp = tmp.left
+		} else {
+			if tmp.left != nil {
+				result += tmp.left.cnt
+			}
+			if tmp.right == nil {
+				return result
+			}
+			tmp = tmp.right
+		}
+	}
+	return result
+}
+
+func (t *BinaryTrie) LowerBoundElm(x int) int {
+	lb := t.LowerBound(x)
+	if lb == 0 {
+		return -1
+	}
+	return t.GetSmallKth(lb)
+}
+
+func (t *BinaryTrie) UpperBound(x int) int {
+	return t.LowerBound(x + 1)
+}
+
+func (t *BinaryTrie) UpperBoundElm(x int) int {
+	return t.LowerBoundElm(x + 1)
+}
+
+func nextInt() int {
 	sc.Scan()
 	i, e := strconv.Atoi(sc.Text())
 	if e != nil {
@@ -25,109 +235,7 @@ func getI() int {
 	return i
 }
 
-func getF() float64 {
-	sc.Scan()
-	i, e := strconv.ParseFloat(sc.Text(), 64)
-	if e != nil {
-		panic(e)
-	}
-	return i
-}
-
-func getInts(N int) []int {
-	ret := make([]int, N)
-	for i := 0; i < N; i++ {
-		ret[i] = getI()
-	}
-	return ret
-}
-
-func getS() string {
+func nextString() string {
 	sc.Scan()
 	return sc.Text()
-}
-
-// min, max, asub, absなど基本関数
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-// min for n entry
-func nmin(a ...int) int {
-	ret := a[0]
-	for _, e := range a {
-		ret = min(ret, e)
-	}
-	return ret
-}
-
-// max for n entry
-func nmax(a ...int) int {
-	ret := a[0]
-	for _, e := range a {
-		ret = max(ret, e)
-	}
-	return ret
-}
-
-func chmin(a *int, b int) bool {
-	if *a < b {
-		return false
-	}
-	*a = b
-	return true
-}
-
-func chmax(a *int, b int) bool {
-	if *a > b {
-		return false
-	}
-	*a = b
-	return true
-}
-
-func asub(a, b int) int {
-	if a > b {
-		return a - b
-	}
-	return b - a
-}
-
-func abs(a int) int {
-	if a >= 0 {
-		return a
-	}
-	return -a
-}
-
-func lowerBound(a []int, x int) int {
-	idx := sort.Search(len(a), func(i int) bool {
-		return a[i] >= x
-	})
-	return idx
-}
-
-func upperBound(a []int, x int) int {
-	idx := sort.Search(len(a), func(i int) bool {
-		return a[i] > x
-	})
-	return idx
-}
-
-func main() {
-	defer wr.Flush()
-	sc.Split(bufio.ScanWords)
-	sc.Buffer([]byte{}, math.MaxInt32)
-	// this template is new version.
-	// use getI(), getS(), getInts(), getF()
 }

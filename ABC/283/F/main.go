c68c8bfc18c441b95d4e5b22bf5d7e0f345eb4f0
aruaru0 <aruaru0@gmail.com>
2022-12-28 22:19:35 +0900

    abc283-F

--- ABC/283/F/main.go
+++ ABC/283/F/main.go
@@ -124,10 +124,181 @@ func upperBound(a []int, x int) int {
 	return idx
 }
 
+type E func() int
+type Merger func(a, b int) int
+type Compare func(v int) bool
+type Segtree struct {
+	n      int
+	size   int
+	log    int
+	d      []int
+	e      E
+	merger Merger
+}
+
+func newSegtree(v []int, e E, m Merger) *Segtree {
+	seg := new(Segtree)
+	seg.n = len(v)
+	seg.log = seg.ceilPow2(seg.n)
+	seg.size = 1 << uint(seg.log)
+	seg.d = make([]int, 2*seg.size)
+	seg.e = e
+	seg.merger = m
+	for i, _ := range seg.d {
+		seg.d[i] = seg.e()
+	}
+	for i := 0; i < seg.n; i++ {
+		seg.d[seg.size+i] = v[i]
+	}
+	for i := seg.size - 1; i >= 1; i-- {
+		seg.Update(i)
+	}
+	return seg
+}
+func (seg *Segtree) Update(k int) {
+	seg.d[k] = seg.merger(seg.d[2*k], seg.d[2*k+1])
+}
+func (seg *Segtree) Set(p, x int) {
+	p += seg.size
+	seg.d[p] = x
+	for i := 1; i <= seg.log; i++ {
+		seg.Update(p >> uint(i))
+	}
+}
+func (seg *Segtree) Get(p int) int {
+	return seg.d[p+seg.size]
+}
+func (seg *Segtree) Prod(l, r int) int {
+	sml, smr := seg.e(), seg.e()
+	l += seg.size
+	r += seg.size
+	for l < r {
+		if (l & 1) == 1 {
+			sml = seg.merger(sml, seg.d[l])
+			l++
+		}
+		if (r & 1) == 1 {
+			r--
+			smr = seg.merger(seg.d[r], smr)
+		}
+		l >>= 1
+		r >>= 1
+	}
+	return seg.merger(sml, smr)
+}
+func (seg *Segtree) AllProd() int {
+	return seg.d[1]
+}
+func (seg *Segtree) MaxRight(l int, cmp Compare) int {
+	if l == seg.n {
+		return seg.n
+	}
+	l += seg.size
+	sm := seg.e()
+	for {
+		for l%2 == 0 {
+			l >>= 1
+		}
+		if !cmp(seg.merger(sm, seg.d[l])) {
+			for l < seg.size {
+				l = 2 * l
+				if cmp(seg.merger(sm, seg.d[l])) {
+					sm = seg.merger(sm, seg.d[l])
+					l++
+				}
+			}
+			return l - seg.size
+		}
+		sm = seg.merger(sm, seg.d[l])
+		l++
+		if l&-l == l {
+			break
+		}
+	}
+	return seg.n
+}
+func (seg *Segtree) MinLeft(r int, cmp Compare) int {
+	if r == 0 {
+		return 0
+	}
+	r += seg.size
+	sm := seg.e()
+	for {
+		r--
+		for r > 1 && r%2 != 0 {
+			r >>= 1
+		}
+		if !cmp(seg.merger(seg.d[r], sm)) {
+			for r < seg.size {
+				r = 2*r + 1
+				if cmp(seg.merger(seg.d[r], sm)) {
+					sm = seg.merger(seg.d[r], sm)
+					r--
+				}
+			}
+			return r + 1 - seg.size
+		}
+		sm = seg.merger(seg.d[r], sm)
+		if r&-r == r {
+			break
+		}
+	}
+	return 0
+}
+func (seg *Segtree) ceilPow2(n int) int {
+	x := 0
+	for (1 << uint(x)) < n {
+		x++
+	}
+	return x
+}
+
+const inf = int(1e18)
+
 func main() {
 	defer wr.Flush()
 	sc.Split(bufio.ScanWords)
 	sc.Buffer([]byte{}, math.MaxInt32)
 	// this template is new version.
 	// use getI(), getS(), getInts(), getF()
+	n := getI()
+	p := getInts(n)
+	for i := 0; i < n; i++ {
+		p[i]--
+	}
+
+	ans := make([]int, n)
+	for i := 0; i < n; i++ {
+		ans[i] = n * 2
+	}
+
+	e := func() int { return -inf }
+	merger := func(a, b int) int { return max(a, b) }
+
+	for ri := 0; ri < 2; ri++ {
+		for rj := 0; rj < 2; rj++ {
+			v := make([]int, n)
+			for i := 0; i < n; i++ {
+				v[i] = -inf
+			}
+			t := newSegtree(v, e, merger)
+			for i := 0; i < n; i++ {
+				x := i + p[i]
+				ans[i] = min(ans[i], x-t.Prod(0, p[i]))
+				t.Set(p[i], x)
+			}
+			for i := 0; i < n; i++ {
+				p[i] = n - 1 - p[i]
+			}
+		}
+		for i := 0; i < n/2; i++ {
+			p[i], p[n-1-i] = p[n-1-i], p[i]
+			ans[i], ans[n-1-i] = ans[n-1-i], ans[i]
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		fmt.Fprint(wr, ans[i], " ")
+	}
+	out()
 }

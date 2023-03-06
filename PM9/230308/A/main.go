5b44e14691b8367fb437f28b8e7fb09d595835fa
aruaru0 <aruaru0@gmail.com>
2023-03-06 22:26:37 +0900

    230308

---
+++ PM9/230308/A/main.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func NextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+func rec(x, n int) int {
+	if n == len(a) {
+		return 0
+	}
+
+	if memo[x][n] != -inf {
+		return memo[x][n]
+	}
+
+	ret := rec(x, n+1)
+
+	p := make([]int, 0)
+	b := make([]int, 0)
+	tmp := x
+	for tmp > 0 {
+		b = append(b, len(p))
+		p = append(p, tmp%10)
+		tmp /= 10
+	}
+
+	for {
+		v := 0
+		for _, e := range b {
+			v = v*10 + p[e]
+		}
+
+		if v >= a[n] {
+			ret = max(ret, rec(v-a[n], n+1)+1)
+		}
+
+		if NextPermutation(sort.IntSlice(b)) != true {
+			break
+		}
+	}
+
+	memo[x][n] = ret
+
+	return ret
+}
+
+var a []int
+var memo [10100][110]int
+
+const inf = int(1e18)
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, X := getI(), getI()
+	a = getInts(N)
+
+	for i := 0; i < 10100; i++ {
+		for j := 0; j < 110; j++ {
+			memo[i][j] = -inf
+		}
+	}
+
+	ret := rec(X, 0)
+	out(ret)
+}

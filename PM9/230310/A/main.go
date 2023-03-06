936a24e50ef19029282974b05750226efb84b149
aruaru0 <aruaru0@gmail.com>
2023-03-06 23:43:52 +0900

    230310

---
+++ PM9/230310/A/main.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func solve(x, k int) {
+	out("--------------------")
+	out(x, k, " = :")
+	for i := 0; i < k; i++ {
+		a := x
+		tot := 0
+		for a > 0 {
+			tot += a % 10
+			a /= 10
+		}
+		x = x - tot
+		out(x, tot)
+	}
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, K := getI(), getI()
+
+	// ダブリング
+	p := make([][]int, 32)
+	for i := 0; i < 32; i++ {
+		p[i] = make([]int, N+1)
+	}
+	for i := 0; i <= N; i++ {
+		a := i
+		tot := 0
+		for a > 0 {
+			tot += a % 10
+			a /= 10
+		}
+		p[0][i] = i - tot
+	}
+
+	for i := 1; i < 32; i++ {
+		for j := 0; j < N+1; j++ {
+			p[i][j] = p[i-1][p[i-1][j]]
+		}
+	}
+
+	for i := 1; i <= N; i++ {
+		pos := i
+		k := K
+		for j := 0; k > 0; j++ {
+			if k%2 == 1 {
+				pos = p[j][pos]
+			}
+			k /= 2
+		}
+		out(pos)
+	}
+
+}

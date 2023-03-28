704bb6c0166d91906a9fb4863211efc654e84e2c
aruaru0 <aruaru0@gmail.com>
2023-03-28 21:48:43 +0900

    abc295-abc

---
+++ ABC/295/B/main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	R, C := getI(), getI()
+	a := make([][]byte, R)
+	b := make([][]byte, R)
+	for i := 0; i < R; i++ {
+		b[i] = []byte(getS())
+		a[i] = make([]byte, C)
+		for j := 0; j < C; j++ {
+			a[i][j] = b[i][j]
+		}
+	}
+
+	for r := 0; r < R; r++ {
+		for c := 0; c < C; c++ {
+			if b[r][c] >= '1' && b[r][c] <= '9' {
+				t := int(b[r][c] - '0')
+				for dr := -t; dr <= t; dr++ {
+					for dc := -t; dc <= t; dc++ {
+						nr, nc := r+dr, c+dc
+						if nr < 0 || nr >= R || nc < 0 || nc >= C {
+							continue
+						}
+						if abs(dr)+abs(dc) <= t {
+							a[nr][nc] = '.'
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for i := 0; i < R; i++ {
+		out(string(a[i]))
+	}
+}

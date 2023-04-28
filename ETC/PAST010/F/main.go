b4db941e165aa858e07fcefcc077e8dee44c0fae
aruaru0 <aruaru0@gmail.com>
2023-04-28 21:15:56 +0900

    PAST010-F

---
+++ ETC/PAST010/F/main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	H, W, N := getI(), getI(), getI()
+	a := make([][]int, H)
+	col := make([][]int, H)
+	for i := 0; i < H; i++ {
+		a[i] = getInts(W)
+		col[i] = make([]int, W)
+	}
+	c := getInts(N)
+
+	for h := 0; h < H; h++ {
+		for w := 0; w < W; w++ {
+			col[h][w] = c[a[h][w]-1]
+		}
+	}
+
+	dh := []int{-1, 1, 0, 0}
+	dw := []int{0, 0, -1, 1}
+	for h := 0; h < H; h++ {
+		for w := 0; w < W; w++ {
+			for i := 0; i < 4; i++ {
+				ph := h + dh[i]
+				pw := w + dw[i]
+				if ph < 0 || ph >= H || pw < 0 || pw >= W {
+					continue
+				}
+				// 色が同じだが都市が違う
+				if col[h][w] == col[ph][pw] && a[h][w] != a[ph][pw] {
+					out("No")
+					return
+				}
+			}
+		}
+	}
+	out("Yes")
+}

260b216d1232c5da37aaedee5a1cc0b0380ab443
aruaru0 <aruaru0@gmail.com>
2023-06-06 13:05:29 +0900

    abc304-D

---
+++ ABC/304/D/main.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type pos struct {
+	x, y int
+}
+
+const inf = int(1e10)
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	W, H := getI(), getI()
+	N := getI()
+	p := make([]pos, N)
+	for i := 0; i < N; i++ {
+		p[i] = pos{getI(), getI()}
+	}
+	A := getI()
+	a := getInts(A)
+	a = append(a, W)
+	B := getI()
+	b := getInts(B)
+	b = append(b, H)
+
+	area := make(map[pos]int)
+	total_area := (A + 1) * (B + 1)
+	for i := 0; i < N; i++ {
+		x, y := p[i].x, p[i].y
+		ia := lowerBound(a, x)
+		ib := lowerBound(b, y)
+		area[pos{ia, ib}]++
+		// out(ia, ib, lowerBound(a, 0))
+	}
+	// out(area, total_area)
+	mi, mx := inf, 0
+	for _, e := range area {
+		mi = min(mi, e)
+		mx = max(mx, e)
+	}
+	if len(area) < total_area {
+		mi = 0
+	}
+	out(mi, mx)
+}

3df4c1956ee83df09f1140aa2c66d404e9896e0c
aruaru0 <aruaru0@gmail.com>
2023-08-10 10:38:43 +0900

    abc313-F

---
+++ ABC/313/F/main.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func swap(x, y *int) {
+	*x, *y = *y, *x
+}
+
+// C++の正解コードをgo言語にしただけ、写経
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+
+	n, m := getI(), getI()
+	a := make([]int, n)
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i], b[i] = getI(), getI()
+	}
+	x := make([]int, m)
+	y := make([]int, m)
+	for i := 0; i < m; i++ {
+		x[i], y[i] = getI()-1, getI()-1
+		if x[i] == y[i] && a[x[i]] < b[x[i]] {
+			swap(&a[x[i]], &b[x[i]])
+		}
+	}
+	ans := 0
+	p := make([]int, 0)
+	q := make([]int, 0)
+	id := make([]int, n)
+	for i := 0; i < n; i++ {
+		ans += a[i] * 2
+		d := abs(a[i] - b[i])
+		if a[i] >= b[i] {
+			id[i] = len(p)
+			p = append(p, d)
+		} else {
+			id[i] = len(q)
+			q = append(q, d)
+		}
+	}
+	ps := len(p)
+	qs := len(q)
+	ls := make([]int, ps)
+	for i := 0; i < m; i++ {
+		xp := (a[x[i]] >= b[x[i]])
+		yp := (a[y[i]] >= b[y[i]])
+		if xp == yp {
+			if !xp {
+				ans += q[id[x[i]]] + q[id[y[i]]]
+				q[id[x[i]]] = 0
+				q[id[y[i]]] = 0
+			}
+		} else {
+			if !xp {
+				swap(&x[i], &y[i])
+			}
+			ls[id[x[i]]] |= 1 << id[y[i]]
+		}
+	}
+	if ps <= qs {
+		mx := 0
+		for bit := 0; bit < (1 << ps); bit++ {
+			now := 0
+			cq := 0
+			for i := 0; i < ps; i++ {
+				if bit>>i&1 == 1 {
+					now -= p[i]
+					cq |= ls[i]
+				}
+			}
+			for i := 0; i < qs; i++ {
+				if cq>>i&1 == 1 {
+					now += q[i]
+				}
+			}
+			mx = max(mx, now)
+		}
+		ans += mx
+	} else {
+		dp := make([][]int, ps+1)
+		for i := 0; i < ps+1; i++ {
+			dp[i] = make([]int, 1<<qs)
+			for j := 0; j < 1<<qs; j++ {
+				dp[i][j] = -1e9
+			}
+		}
+		dp[0][0] = 0
+		for i := 0; i < ps; i++ {
+			for j := 0; j < (1 << qs); j++ {
+				dp[i+1][j] = max(dp[i+1][j], dp[i][j])
+				dp[i+1][j|ls[i]] = max(dp[i+1][j|ls[i]], dp[i][j]-p[i])
+			}
+		}
+		mx := 0
+		for j := 0; j < (1 << qs); j++ {
+			now := dp[ps][j]
+			for i := 0; i < qs; i++ {
+				if (j >> i & 1) == 1 {
+					now += q[i]
+				}
+			}
+			mx = max(mx, now)
+		}
+		ans += mx
+	}
+	out(float32(ans) / 2)
+}

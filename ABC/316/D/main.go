1a296612471724d7bd64f1500f4bc70ce8f3dbf8
aruaru0 <aruaru0@gmail.com>
2023-08-28 09:56:58 +0900

    abc316-D

---
+++ ABC/316/D/main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+const inf = int(1e18)
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+	x := make([]int, N)
+	y := make([]int, N)
+	z := make([]int, N)
+
+	tot := 0
+	for i := 0; i < N; i++ {
+		x[i], y[i], z[i] = getI(), getI(), getI()
+		tot += z[i]
+	}
+
+	dp := make([]int, tot+1)
+	for i := 0; i < tot+1; i++ {
+		dp[i] = inf
+	}
+	dp[0] = 0
+	for i := 0; i < N; i++ {
+		xx, yy, zz := x[i], y[i], z[i]
+		win := max(0, (xx+yy)/2+1-xx) // エリアで勝つために必要な得票数
+		for j := tot; j >= 0; j-- {
+			if j-zz < 0 {
+				continue
+			}
+			dp[j] = min(dp[j], dp[j-zz]+win)
+		}
+		// out(xx, yy, zz)
+		// out(dp)
+	}
+
+	ans := inf
+	for i := tot/2 + 1; i <= tot; i++ {
+		ans = min(ans, dp[i])
+	}
+	out(ans)
+
+}

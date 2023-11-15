ab521e3522a9ad4e5d3b97f142d18c6f7f38e62f
aruaru0 <aruaru0@gmail.com>
2023-11-15 11:43:06 +0900

    231115-A

---
+++ PM9/231115/A/main.go
@@ -0,0 +1,245 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	// for i := 0; i < 100; i++ {
+	// 	x, y := getF(), getF()
+	// 	out("{", x, ",", y, "},")
+	// }
+
+	dat := [][2]float64{
+		{-0.63, -0.63},
+		{-0.63, -0.49},
+		{-0.63, -0.35},
+		{-0.63, -0.21},
+		{-0.63, -0.07},
+		{-0.63, 0.07},
+		{-0.63, 0.21},
+		{-0.63, 0.35},
+		{-0.63, 0.49},
+		{-0.63, 0.63},
+		{-0.49, -0.63},
+		{-0.49, -0.49},
+		{-0.49, -0.35},
+		{-0.49, -0.21},
+		{-0.49, -0.07},
+		{-0.49, 0.07},
+		{-0.49, 0.21},
+		{-0.49, 0.35},
+		{-0.49, 0.49},
+		{-0.49, 0.63},
+		{-0.35, -0.63},
+		{-0.35, -0.49},
+		{-0.35, -0.35},
+		{-0.35, -0.21},
+		{-0.35, -0.07},
+		{-0.35, 0.07},
+		{-0.35, 0.21},
+		{-0.35, 0.35},
+		{-0.35, 0.49},
+		{-0.35, 0.63},
+		{-0.21, -0.63},
+		{-0.21, -0.49},
+		{-0.21, -0.35},
+		{-0.21, -0.21},
+		{-0.21, -0.07},
+		{-0.21, 0.07},
+		{-0.21, 0.21},
+		{-0.21, 0.35},
+		{-0.21, 0.49},
+		{-0.21, 0.63},
+		{-0.07, -0.63},
+		{-0.07, -0.49},
+		{-0.07, -0.35},
+		{-0.07, -0.21},
+		{-0.07, -0.07},
+		{-0.07, 0.07},
+		{-0.07, 0.21},
+		{-0.07, 0.35},
+		{-0.07, 0.49},
+		{-0.07, 0.63},
+		{0.07, -0.63},
+		{0.07, -0.49},
+		{0.07, -0.35},
+		{0.07, -0.21},
+		{0.07, -0.07},
+		{0.07, 0.07},
+		{0.07, 0.21},
+		{0.07, 0.35},
+		{0.07, 0.49},
+		{0.07, 0.63},
+		{0.21, -0.63},
+		{0.21, -0.49},
+		{0.21, -0.35},
+		{0.21, -0.21},
+		{0.21, -0.07},
+		{0.21, 0.07},
+		{0.21, 0.21},
+		{0.21, 0.35},
+		{0.21, 0.49},
+		{0.21, 0.63},
+		{0.35, -0.63},
+		{0.35, -0.49},
+		{0.35, -0.35},
+		{0.35, -0.21},
+		{0.35, -0.07},
+		{0.35, 0.07},
+		{0.35, 0.21},
+		{0.35, 0.35},
+		{0.35, 0.49},
+		{0.35, 0.63},
+		{0.49, -0.63},
+		{0.49, -0.49},
+		{0.49, -0.35},
+		{0.49, -0.21},
+		{0.49, -0.07},
+		{0.49, 0.07},
+		{0.49, 0.21},
+		{0.49, 0.35},
+		{0.49, 0.49},
+		{0.49, 0.63},
+		{0.63, -0.63},
+		{0.63, -0.49},
+		{0.63, -0.35},
+		{0.63, -0.21},
+		{0.63, -0.07},
+		{0.63, 0.07},
+		{0.63, 0.21},
+		{0.63, 0.35},
+		{0.63, 0.49},
+		{0.63, 0.63},
+	}
+
+	for _, e := range dat {
+		out(e[0], e[1])
+	}
+
+}

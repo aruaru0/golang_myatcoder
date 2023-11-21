3debfd2438cfdffe59aa06f0585eb92f1a17c08e
aruaru0 <aruaru0@gmail.com>
2023-11-21 09:31:07 +0900

    abc329-E

---
+++ ABC/329/E/main.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, M := getI(), getI()
+	s := []byte(getS())
+	t := []byte(getS())
+
+	used := make([]bool, N)
+	q := make([]int, 0)
+
+	push := func(i int) {
+		if i < 0 || i+M > N {
+			return
+		}
+		if used[i] {
+			return
+		}
+		for j := 0; j < M; j++ {
+			if s[i+j] != '?' && s[i+j] != t[j] {
+				return
+			}
+		}
+		used[i] = true
+		q = append(q, i)
+	}
+
+	for i := 0; i < N-M+1; i++ {
+		push(i)
+	}
+
+	// out(q)
+	for len(q) != 0 {
+		i := q[0]
+		q = q[1:]
+		for j := 0; j < M; j++ {
+			s[i+j] = '?'
+		}
+		for ni := i - M + 1; ni <= i+M-1; ni++ {
+			push(ni)
+		}
+		// out(q, string(s))
+	}
+
+	ok := true
+	for i := 0; i < N; i++ {
+		if s[i] != '?' {
+			ok = false
+		}
+	}
+	if ok {
+		out("Yes")
+	} else {
+		out("No")
+	}
+}

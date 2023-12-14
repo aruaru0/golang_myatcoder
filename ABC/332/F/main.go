c175921c212a12e8c507db2af3dcb08787db2780
aruaru0 <aruaru0@gmail.com>
2023-12-14 10:26:15 +0900

    abc332-F

---
+++ ABC/332/F/main.go
@@ -0,0 +1,616 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type S struct {
+	v int
+}
+type F struct {
+	a, b int
+}
+type E func() S
+type Merger func(a, b S) S
+type Mapper func(f F, x S) S
+type Comp func(f, g F) F
+type Id func() F
+type Compare func(v S) bool
+type LazySegtree struct {
+	n      int
+	size   int
+	log    int
+	d      []S
+	lz     []F
+	e      E
+	merger Merger
+	mapper Mapper
+	comp   Comp
+	id     Id
+}
+
+func newLazySegtree(v []S, e E, merger Merger, mapper Mapper, comp Comp, id Id) *LazySegtree {
+	lseg := new(LazySegtree)
+	lseg.n = len(v)
+	lseg.log = lseg.ceilPow2(lseg.n)
+	lseg.size = 1 << uint(lseg.log)
+	lseg.d = make([]S, 2*lseg.size)
+	lseg.e = e
+	lseg.lz = make([]F, lseg.size)
+	lseg.merger = merger
+	lseg.mapper = mapper
+	lseg.comp = comp
+	lseg.id = id
+	for i, _ := range lseg.d {
+		lseg.d[i] = lseg.e()
+	}
+	for i, _ := range lseg.lz {
+		lseg.lz[i] = lseg.id()
+	}
+	for i := 0; i < lseg.n; i++ {
+		lseg.d[lseg.size+i] = v[i]
+	}
+	for i := lseg.size - 1; i >= 1; i-- {
+		lseg.Update(i)
+	}
+	return lseg
+}
+func (lseg *LazySegtree) Update(k int) {
+	lseg.d[k] = lseg.merger(lseg.d[2*k], lseg.d[2*k+1])
+}
+func (lseg *LazySegtree) AllApply(k int, f F) {
+	lseg.d[k] = lseg.mapper(f, lseg.d[k])
+	if k < lseg.size {
+		lseg.lz[k] = lseg.comp(f, lseg.lz[k])
+	}
+}
+func (lseg *LazySegtree) Push(k int) {
+	lseg.AllApply(2*k, lseg.lz[k])
+	lseg.AllApply(2*k+1, lseg.lz[k])
+	lseg.lz[k] = lseg.id()
+}
+func (lseg *LazySegtree) Set(p int, x S) {
+	p += lseg.size
+	for i := lseg.log; i <= 1; i-- {
+		lseg.Push(p >> uint(i))
+	}
+	lseg.d[p] = x
+	for i := 1; i <= lseg.log; i++ {
+		lseg.Update(p >> uint(i))
+	}
+}
+func (lseg *LazySegtree) Get(p int) S {
+	p += lseg.size
+	for i := lseg.log; i >= 1; i-- {
+		lseg.Push(p >> uint(i))
+	}
+	return lseg.d[p]
+}
+func (lseg *LazySegtree) Prod(l, r int) S {
+	if l == r {
+		return lseg.e()
+	}
+	l += lseg.size
+	r += lseg.size
+	for i := lseg.log; i >= 1; i-- {
+		if (l>>uint(i))<<uint(i) != l {
+			lseg.Push(l >> uint(i))
+		}
+		if (r>>uint(i))<<uint(i) != r {
+			lseg.Push(r >> uint(i))
+		}
+	}
+	sml, smr := lseg.e(), lseg.e()
+	for l < r {
+		if (l & 1) == 1 {
+			sml = lseg.merger(sml, lseg.d[l])
+			l++
+		}
+		if (r & 1) == 1 {
+			r--
+			smr = lseg.merger(lseg.d[r], smr)
+		}
+		l >>= 1
+		r >>= 1
+	}
+	return lseg.merger(sml, smr)
+}
+func (lseg *LazySegtree) AllProd() S {
+	return lseg.d[1]
+}
+func (lseg *LazySegtree) Apply(p int, f F) {
+	p += lseg.size
+	for i := lseg.log; i >= 1; i-- {
+		lseg.Push(p >> uint(i))
+	}
+	lseg.d[p] = lseg.mapper(f, lseg.d[p])
+	for i := 1; i <= lseg.log; i++ {
+		lseg.Update(p >> uint(i))
+	}
+}
+func (lseg *LazySegtree) RangeApply(l int, r int, f F) {
+	if l == r {
+		return
+	}
+	l += lseg.size
+	r += lseg.size
+	for i := lseg.log; i >= 1; i-- {
+		if (l>>uint(i))<<uint(i) != l {
+			lseg.Push(l >> uint(i))
+		}
+		if (r>>uint(i))<<uint(i) != r {
+			lseg.Push((r - 1) >> uint(i))
+		}
+	}
+	l2, r2 := l, r
+	for l < r {
+		if l&1 == 1 {
+			lseg.AllApply(l, f)
+			l++
+		}
+		if r&1 == 1 {
+			r--
+			lseg.AllApply(r, f)
+		}
+		l >>= 1
+		r >>= 1
+	}
+	l, r = l2, r2
+	for i := 1; i <= lseg.log; i++ {
+		if (l>>uint(i))<<uint(i) != l {
+			lseg.Update(l >> uint(i))
+		}
+		if (r>>uint(i))<<uint(i) != r {
+			lseg.Update((r - 1) >> uint(i))
+		}
+	}
+}
+func (lseg *LazySegtree) MaxRight(l int, cmp Compare) int {
+	if l == lseg.n {
+		return lseg.n
+	}
+	l += lseg.size
+	for i := lseg.log; i >= 1; i-- {
+		lseg.Push(l >> uint(i))
+	}
+	sm := lseg.e()
+	for {
+		for l%2 == 0 {
+			l >>= 1
+		}
+		if !cmp(lseg.merger(sm, lseg.d[l])) {
+			for l < lseg.size {
+				lseg.Push(l)
+				l = 2 * l
+				if cmp(lseg.merger(sm, lseg.d[l])) {
+					sm = lseg.merger(sm, lseg.d[l])
+					l++
+				}
+			}
+			return l - lseg.size
+		}
+		sm = lseg.merger(sm, lseg.d[l])
+		l++
+		if l&-l == l {
+			break
+		}
+	}
+	return lseg.n
+}
+func (lseg *LazySegtree) MinLeft(r int, cmp Compare) int {
+	if r == 0 {
+		return 0
+	}
+	r += lseg.size
+	for i := lseg.log; i >= 1; i-- {
+		lseg.Push(r - 1>>uint(i))
+	}
+	sm := lseg.e()
+	for {
+		r--
+		for r > 1 && r%2 != 0 {
+			r >>= 1
+		}
+		if !cmp(lseg.merger(lseg.d[r], sm)) {
+			for r < lseg.size {
+				lseg.Push(r)
+				r = 2*r + 1
+				if cmp(lseg.merger(lseg.d[r], sm)) {
+					sm = lseg.merger(lseg.d[r], sm)
+					r--
+				}
+			}
+			return r + 1 - lseg.size
+		}
+		sm = lseg.merger(lseg.d[r], sm)
+		if r&-r == r {
+			break
+		}
+	}
+	return 0
+}
+func (lseg *LazySegtree) ceilPow2(n int) int {
+	x := 0
+	for (1 << uint(x)) < n {
+		x++
+	}
+	return x
+}
+
+//----------------------------------------
+// modint
+//----------------------------------------
+type modint struct {
+	mod       int
+	factMemo  []int
+	ifactMemo []int
+}
+
+func newModint(m int) *modint {
+	var ret modint
+	ret.mod = m
+	ret.factMemo = []int{1, 1}
+	ret.ifactMemo = []int{1, 1}
+	return &ret
+}
+
+func (m *modint) add(a, b int) int {
+	ret := (a + b) % m.mod
+	if ret < 0 {
+		ret += m.mod
+	}
+	return ret
+}
+
+func (m *modint) sub(a, b int) int {
+	ret := (a - b) % m.mod
+	if ret < 0 {
+		ret += m.mod
+	}
+	return ret
+}
+
+func (m *modint) mul(a, b int) int {
+	a %= m.mod
+	b %= m.mod
+	ret := a * b % m.mod
+	if ret < 0 {
+		ret += m.mod
+	}
+	return ret
+}
+
+func (m *modint) div(a, b int) int {
+	a %= m.mod
+	ret := a * m.modinv(b)
+	ret %= m.mod
+	return ret
+}
+
+func (m *modint) pow(p, n int) int {
+	ret := 1
+	x := p % m.mod
+	for n != 0 {
+		if n%2 == 1 {
+			ret *= x
+			ret %= m.mod
+		}
+		n /= 2
+		x = x * x % m.mod
+	}
+	return ret
+}
+
+// 逆元を使った割り算（MOD）
+// mod. m での a の逆元 a^{-1} を計算する
+// func (m *modint) modinv(a int) int {
+// 	b := m.mod
+// 	u := 1
+// 	v := 0
+// 	for b != 0 {
+// 		t := a / b
+// 		a -= t * b
+// 		a, b = b, a
+// 		u -= t * v
+// 		u, v = v, u
+// 	}
+// 	u %= m.mod
+// 	if u < 0 {
+// 		u += m.mod
+// 	}
+// 	return u
+// }
+
+// 拡張オイラーの互除法で逆元を求める
+func (mm *modint) modinv(a int) int {
+	m := mm.mod
+	b, u, v := m, 1, 0
+	for b != 0 {
+		t := a / b
+		a -= t * b
+		a, b = b, a
+		u -= t * v
+		u, v = v, u
+	}
+	u %= m
+	if u < 0 {
+		u += m
+	}
+	return u
+}
+
+//-----------------------------------------------
+// 行列累乗
+// 　A[][]のp乗を求める
+//-----------------------------------------------
+func (m *modint) powModMatrix(A [][]int, p int) [][]int {
+	N := len(A)
+	ret := make([][]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = make([]int, N)
+		ret[i][i] = 1
+	}
+
+	for p > 0 {
+		if p&1 == 1 {
+			ret = m.mulMod(ret, A)
+		}
+		A = m.mulMod(A, A)
+		p >>= 1
+	}
+
+	return ret
+}
+
+func (m *modint) mulMod(A, B [][]int) [][]int {
+	H := len(A)
+	W := len(B[0])
+	K := len(A[0])
+	C := make([][]int, W)
+	for i := 0; i < W; i++ {
+		C[i] = make([]int, W)
+	}
+
+	for i := 0; i < H; i++ {
+		for j := 0; j < W; j++ {
+			for k := 0; k < K; k++ {
+				C[i][j] += A[i][k] * B[k][j]
+				C[i][j] %= m.mod
+			}
+		}
+	}
+
+	return C
+}
+
+//---------------------------------------------------
+// nCk 計算関連：　TELすることがあるかも
+//                ※pow(x, p-2)を何度も取るので
+// 厳しそうな場合は、ここを削除して高速なのを使う
+//---------------------------------------------------
+func (m *modint) mfact(n int) int {
+	if len(m.factMemo) > n {
+		return m.factMemo[n]
+	}
+	if len(m.factMemo) == 0 {
+		m.factMemo = append(m.factMemo, 1)
+	}
+	for len(m.factMemo) <= n {
+		size := len(m.factMemo)
+		m.factMemo = append(m.factMemo, m.factMemo[size-1]*size%m.mod)
+	}
+	return m.factMemo[n]
+}
+
+func (m *modint) mifact(n int) int {
+	if len(m.ifactMemo) > n {
+		return m.ifactMemo[n]
+	}
+	if len(m.ifactMemo) == 0 {
+		m.factMemo = append(m.ifactMemo, 1)
+	}
+	for len(m.ifactMemo) <= n {
+		size := len(m.ifactMemo)
+		m.ifactMemo = append(m.ifactMemo, m.ifactMemo[size-1]*m.pow(size, m.mod-2)%m.mod)
+	}
+	return m.ifactMemo[n]
+}
+
+func (m *modint) nCr(n, r int) int {
+	if n == r {
+		return 1
+	}
+	if n < r || r < 0 {
+		return 0
+	}
+	ret := 1
+	ret *= m.mfact(n)
+	ret %= m.mod
+	ret *= m.mifact(r)
+	ret %= m.mod
+	ret *= m.mifact(n - r)
+	ret %= m.mod
+	return (ret)
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, M := getI(), getI()
+
+	mod := newModint(998244353)
+
+	a := make([]S, N)
+	for i := 0; i < N; i++ {
+		a[i] = S{getI()}
+	}
+
+	e := func() S { return S{0} }
+	merger := func(a, b S) S {
+		return S{mod.add(a.v, b.v)}
+	}
+
+	// 1 - p = f.a
+	// p * x = f.b
+	// v = x * f.a + f.b
+	mapper := func(f F, x S) S {
+		v := mod.add(mod.mul(x.v, f.a), f.b)
+		return S{v}
+
+	}
+
+	// v = x * f.a + f.b
+	// v' = (x * f.a + f.b)*g.a + g.b
+	//    = x * f.a * g.b + f.b*g.a + g.b
+	//          ---------   -------------
+	comp := func(f, g F) F {
+		a := mod.mul(f.a, g.a)
+		b := mod.add(mod.mul(f.a, g.b), f.b)
+		return F{a, b}
+	}
+	id := func() F {
+		return F{1, 0}
+	}
+
+	seg := newLazySegtree(a, e, merger, mapper, comp, id)
+
+	for i := 0; i < M; i++ {
+		l, r, x := getI()-1, getI(), getI()
+		p := mod.div(1, r-l)
+		q := mod.sub(1, p)
+		p = mod.mul(p, x)
+		seg.RangeApply(l, r, F{q, p})
+		// for j := 0; j < N; j++ {
+		// 	seg.Get(j)
+		// }
+	}
+
+	for i := 0; i < N; i++ {
+		ans := seg.Get(i)
+		fmt.Fprint(wr, ans.v, " ")
+	}
+	out()
+}

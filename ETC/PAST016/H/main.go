3ce63e2660944ecd9134c889bbda840118c497d0
aruaru0 <aruaru0@gmail.com>
2024-02-17 09:36:01 +0900

    PAST016-H

---
+++ ETC/PAST016/H/main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+const inf = int(1e18)
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, M := getI(), getI()
+	a := getInts(N)
+
+	// dp[i][j][k] ... i日目までで前日j日宿題をした場合の楽しさの合計（kは前日宿題をしたかどうか）
+	dp := make([][][2]int, N+1)
+	for i := 0; i <= N; i++ {
+		dp[i] = make([][2]int, M+1)
+		for j := 0; j <= M; j++ {
+			dp[i][j][0] = -inf
+			dp[i][j][1] = -inf
+		}
+	}
+	dp[0][0][0] = 0
+	for i := 0; i < N; i++ {
+		for j := 0; j <= min(i+1, M); j++ {
+			dp[i+1][j][0] = max(dp[i][j][0]+a[i], dp[i][j][1]+a[i])
+			if j != 0 {
+				dp[i+1][j][1] = dp[i][j-1][0]
+			}
+		}
+		// out(dp[i+1])
+	}
+	out(max(dp[N][M][0], dp[N][M][1]))
+}

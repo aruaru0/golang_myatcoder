a1b5950fba25c0a0fda3842e95aedeb827980007
aruaru0 <aruaru0@gmail.com>
2024-02-28 09:36:18 +0900

    abc342-E

---
+++ ABC/342/E/main.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type edge struct {
+	to, l, d, k, c int
+}
+
+type pqi struct{ a, to int }
+
+type priorityQueue []pqi
+
+func (pq priorityQueue) Len() int            { return len(pq) }
+func (pq priorityQueue) Swap(i, j int)       { pq[i], pq[j] = pq[j], pq[i] }
+func (pq priorityQueue) Less(i, j int) bool  { return pq[i].a > pq[j].a }
+func (pq *priorityQueue) Push(x interface{}) { *pq = append(*pq, x.(pqi)) }
+func (pq *priorityQueue) Pop() interface{} {
+	x := (*pq)[len(*pq)-1]
+	*pq = (*pq)[0 : len(*pq)-1]
+	return x
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, M := getI(), getI()
+
+	node := make([][]edge, N)
+	for i := 0; i < M; i++ {
+		l, d, k, c, a, b := getI(), getI(), getI(), getI(), getI()-1, getI()-1
+		node[b] = append(node[b], edge{a, l, d, k, c})
+	}
+
+	const inf = int(2e18)
+	pq := priorityQueue{} // 優先キューは降順ソート
+	heap.Push(&pq, pqi{inf, N - 1})
+	a := make([]int, N)
+	for i := 0; i < N; i++ {
+		a[i] = -inf
+	}
+	// 現在時刻はinfに設定
+	a[N-1] = inf
+
+	for len(pq) != 0 {
+		cur := pq[0]
+		heap.Pop(&pq)
+		if cur.a < a[cur.to] {
+			continue
+		}
+		for _, e := range node[cur.to] {
+			if e.l+e.c <= cur.a {
+				// 現在時刻 -　始発と乗車時間：電車に乗れる範囲の区間を計算
+				x := cur.a - (e.l + e.c)
+				// 始発 + （一番最後の列車、時間範囲で乗れる最後の列車)
+				nt := e.l + min(e.k-1, x/e.d)*e.d
+				// もっと遅い電車に乗れるのであれば、キューに追加
+				if a[e.to] < nt {
+					a[e.to] = nt
+					heap.Push(&pq, pqi{nt, e.to})
+				}
+			}
+		}
+	}
+
+	for i := 0; i < N-1; i++ {
+		if a[i] == -inf {
+			out("Unreachable")
+		} else {
+			out(a[i])
+		}
+	}
+
+}

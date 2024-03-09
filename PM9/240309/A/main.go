f011ae65c65abab535fa1891451f0230e6fbff4e
aruaru0 <aruaru0@gmail.com>
2024-03-09 09:37:22 +0900

    240309

---
+++ PM9/240309/A/main.go
@@ -0,0 +1,215 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func combinations(list []int, k, buf int) (c chan []int) {
+	c = make(chan []int, buf)
+	n := len(list)
+
+	pattern := make([]int, k)
+
+	var body func(pos, begin int)
+	body = func(pos, begin int) {
+		if pos == k {
+			t := make([]int, k)
+			copy(t, pattern)
+			c <- t
+			return
+		}
+
+		for num := begin; num < n+pos-k+1; num++ {
+			pattern[pos] = list[num]
+			body(pos+1, num+1)
+		}
+	}
+	go func() {
+		defer close(c)
+		body(0, 0)
+	}()
+
+	return
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	n := getI()
+	s := getS()
+
+	ans := 4
+	utpc := "UTPC"
+	for i := 0; i < n-3; i++ {
+		t := 4
+		// 区間の文字種類を調べる
+		stk := make(map[byte]bool)
+		for j := 0; j < 4; j++ {
+			stk[s[i+j]] = true
+		}
+		// 最初から一致している数を調べる
+		if s[i] == 'U' {
+			t--
+		}
+		if s[i+1] == 'T' {
+			t--
+		}
+		if s[i+2] == 'P' {
+			t--
+		}
+		if s[i+3] == 'C' {
+			t--
+		}
+		// 内部でスワップして一致する量を調べる
+		for j := 0; j < 3; j++ {
+			for k := j + 1; k < 4; k++ {
+				if s[i+j] == utpc[k] && s[i+k] == utpc[j] {
+					t--
+				}
+			}
+		}
+		// もし文字種類が4でt==4の時はt=3
+		if len(stk) == 4 && t == 4 {
+			t = 3
+		}
+
+		for j := 0; j < 2; j++ {
+			for k := j + 1; k < 4; k++ {
+				for l := j + 1; l < 4; l++ {
+					if k == l {
+						continue
+					}
+					if s[i+j] == utpc[k] && s[i+k] == utpc[l] && s[i+l] == utpc[j] {
+						t--
+					}
+				}
+			}
+		}
+		ans = min(t, ans)
+	}
+
+	out(ans)
+}

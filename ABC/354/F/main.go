32509489075c0c20207f488d2ed877d24d10813b
aruaru0 <aruaru0@gmail.com>
2024-05-23 08:16:34 +0900

    abc354-F

---
+++ ABC/354/F/main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+const inf = int(1e18)
+
+func lis(a []int) []int {
+	n := len(a)
+	dp := make([]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = inf
+	}
+	l := make([]int, 0)
+	for i := 0; i < n; i++ {
+		pos := lowerBound(dp, a[i])
+		dp[pos] = a[i]
+		l = append(l, pos+1)
+	}
+	return l
+}
+
+func solve() {
+	N := getI()
+	a := getInts(N)
+
+	l := lis(a)
+	b := make([]int, N)
+	for i, e := range a {
+		b[N-1-i] = -e
+	}
+	r := lis(b)
+
+	m := nmax(l...)
+	ans := []int{}
+	for i := 0; i < N; i++ {
+		if l[i]+r[N-i-1]-1 == m {
+			ans = append(ans, i+1)
+		}
+	}
+	// out(l, r)
+	out(len(ans))
+	for i := 0; i < len(ans); i++ {
+		fmt.Fprint(wr, ans[i], " ")
+	}
+	out()
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	T := getI()
+	for i := 0; i < T; i++ {
+		solve()
+	}
+}

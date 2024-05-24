2baea46843f82d1860466f3e0ec054c9abea1aa2
aruaru0 <aruaru0@gmail.com>
2024-05-25 08:47:15 +0900

    PAST017-F

---
+++ ETC/PAST017/F/main.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+const mod = 998244353
+
+var node [][]int
+var s []int
+
+func dfs(cur int) int {
+	if len(node[cur]) == 0 {
+		return s[cur]
+	}
+	ret := 0
+	if s[cur] == -1 {
+		ret = 0
+		for _, e := range node[cur] {
+			ret += dfs(e)
+			ret %= mod
+		}
+	} else if s[cur] == -2 {
+		ret = 1
+		for _, e := range node[cur] {
+			ret *= dfs(e)
+			ret %= mod
+		}
+	}
+	return ret
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+	p := getInts(N - 1)
+
+	node = make([][]int, N)
+	for i := 0; i < N-1; i++ {
+		parent := p[i] - 1
+		node[parent] = append(node[parent], i+1)
+	}
+
+	s = make([]int, N)
+	for i := 0; i < N; i++ {
+		t := getS()
+		if t == "+" {
+			s[i] = -1
+		} else if t == "x" {
+			s[i] = -2
+		} else {
+			v, _ := strconv.Atoi(t)
+			s[i] = v
+		}
+	}
+
+	ret := dfs(0)
+	out(ret)
+}

458d8a50f20914c75265625f3000ab5023391485
aruaru0 <aruaru0@gmail.com>
2024-06-10 01:13:39 +0900

    abc357-C

---
+++ ABC/357/C/main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+var p [][]int
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+
+	n := 1
+	for i := 0; i < N; i++ {
+		n *= 3
+	}
+
+	p = make([][]int, n)
+	for i := 0; i < n; i++ {
+		p[i] = make([]int, n)
+	}
+
+	v := 1
+	for level := 0; level < N; level++ {
+		for i := 0; i < n; i++ {
+			for j := 0; j < n; j++ {
+				x, y := i/v, j/v
+				if x%3 == 1 && y%3 == 1 {
+					p[i][j] = 1
+				}
+			}
+		}
+		v *= 3
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if p[i][j] == 1 {
+				fmt.Fprint(wr, ".")
+			} else {
+				fmt.Fprint(wr, "#")
+			}
+		}
+		fmt.Fprintln(wr)
+	}
+}

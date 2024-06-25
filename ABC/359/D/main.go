70e96ae84e0b87aca3b426d38d5e4597bc261433
aruaru0 <aruaru0@gmail.com>
2024-06-25 10:06:18 +0900

    abc359-D

---
+++ ABC/359/D/main.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func reverse(s string) string {
+	rs := []rune(s)
+	for i := 0; i < len(rs)/2; i++ {
+		rs[i], rs[len(rs)-1-i] = rs[len(rs)-1-i], rs[i]
+	}
+	return string(rs)
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	_, K := getI(), getI()
+	s := getS()
+
+	const mod = 998244353
+	dp := make(map[string]int)
+	dp[strings.Repeat(".", K-1)] = 1
+	for _, e := range s {
+		tmp := make(map[string]int)
+		if e != 'B' {
+			for k, v := range dp {
+				tmp[k+"A"] = v
+			}
+		}
+		if e != 'A' {
+			for k, v := range dp {
+				tmp[k+"B"] = v
+			}
+		}
+		// out("---", string(e))
+		// out(tmp)
+		dp = make(map[string]int)
+		for k, v := range tmp {
+			if k == reverse(k) {
+				continue
+			}
+			dp[k[1:]] += v % mod
+		}
+	}
+
+	ans := 0
+	for _, v := range dp {
+		ans += v
+		ans %= mod
+	}
+	out(ans)
+}

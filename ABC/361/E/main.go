8f409d0b0100997f5eb1b3081479aaf3095daed6
aruaru0 <aruaru0@gmail.com>
2024-07-11 21:10:26 +0900

    abc361-E

---
+++ ABC/361/E/main.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type Edge struct {
+	to, cost int
+}
+
+type pqi struct{ cost, to int }
+
+type priorityQueue []pqi
+
+func (pq priorityQueue) Len() int            { return len(pq) }
+func (pq priorityQueue) Swap(i, j int)       { pq[i], pq[j] = pq[j], pq[i] }
+func (pq priorityQueue) Less(i, j int) bool  { return pq[i].cost < pq[j].cost }
+func (pq *priorityQueue) Push(x interface{}) { *pq = append(*pq, x.(pqi)) }
+func (pq *priorityQueue) Pop() interface{} {
+	x := (*pq)[len(*pq)-1]
+	*pq = (*pq)[0 : len(*pq)-1]
+	return x
+}
+
+const inf = int(1e18)
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N := getI()
+	node := make([][]Edge, N)
+	tot := 0
+	for i := 0; i < N-1; i++ {
+		a, b, c := getI()-1, getI()-1, getI()
+		node[a] = append(node[a], Edge{b, c})
+		node[b] = append(node[b], Edge{a, c})
+		tot += c * 2
+	}
+
+	dist := make([]int, N)
+	for i := 0; i < N; i++ {
+		dist[i] = inf
+	}
+	dist[0] = 0
+	pq := priorityQueue{{0, 0}}
+	for len(pq) != 0 {
+		cur := pq[0]
+		heap.Pop(&pq)
+		if dist[cur.to] < cur.cost {
+			continue
+		}
+		for _, e := range node[cur.to] {
+			if dist[e.to] > dist[cur.to]+e.cost {
+				dist[e.to] = dist[cur.to] + e.cost
+				heap.Push(&pq, pqi{dist[e.to], e.to})
+			}
+		}
+	}
+
+	// out(dist)
+
+	d, pos := 0, 0
+	for i := 0; i < N; i++ {
+		if dist[i] > d {
+			pos = i
+			d = dist[i]
+		}
+	}
+
+	for i := 0; i < N; i++ {
+		dist[i] = inf
+	}
+	dist[pos] = 0
+	pq = priorityQueue{{0, pos}}
+	for len(pq) != 0 {
+		cur := pq[0]
+		heap.Pop(&pq)
+		if dist[cur.to] < cur.cost {
+			continue
+		}
+		for _, e := range node[cur.to] {
+			if dist[e.to] > dist[cur.to]+e.cost {
+				dist[e.to] = dist[cur.to] + e.cost
+				heap.Push(&pq, pqi{dist[e.to], e.to})
+			}
+		}
+	}
+
+	mx := 0
+	for i := 0; i < N; i++ {
+		mx = max(mx, dist[i])
+	}
+	out(tot - mx)
+}

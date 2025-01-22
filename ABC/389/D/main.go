a3cfa3f35bdd619823fa24407ecc24b7368883b6
aruaru0 <aruaru0@gmail.com>
2025-01-22 15:20:15 +0900

    abc389-D

---
+++ ABC/389/D/main.go
@@ -0,0 +1,187 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func outSlice[T any](s []T) {
+	for i := 0; i < len(s)-1; i++ {
+		fmt.Fprint(wr, s[i], " ")
+	}
+	fmt.Fprintln(wr, s[len(s)-1])
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func f(y, R int) int {
+	dx := []int{-1, 1, -1, 1}
+	dy := []int{-1, -1, 1, 1}
+
+	ok, ng := 0, R
+	for ok+1 != ng {
+		x := (ok + ng) / 2
+		x *= 2
+		flg := true
+		for i := 0; i < 4; i++ {
+			px, py := x+dx[i], y+dy[i]
+			out("xy", x, y, "pxpy", px, py)
+			if px*px+py*py > 4*R*R {
+				flg = false
+			}
+		}
+		if !flg {
+			ng = x / 2
+		} else {
+			ok = x / 2
+		}
+		out(ok, ng, x, flg)
+	}
+
+	return ok
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	r := getI()
+
+	inside := func(x, y int) bool {
+		x = x*2 + 1
+		y = y*2 + 1
+		return x*x+y*y < r*r*4
+	}
+
+	ans := 0
+	x := 0
+	for y := r; y >= 0; y-- {
+		for inside(x+1, y) {
+			x++
+		}
+		ans += x
+	}
+	ans *= 4
+	ans++
+	out(ans)
+
+}

43c4e528ce1c0117ebf950afa7df5ca09a2a09ed
aruaru0 <aruaru0@gmail.com>
2025-01-30 09:15:09 +0900

    abc390-E

---
+++ ABC/390/E/main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func outSlice[T any](s []T) {
+	for i := 0; i < len(s)-1; i++ {
+		fmt.Fprint(wr, s[i], " ")
+	}
+	fmt.Fprintln(wr, s[len(s)-1])
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+const M = 5000
+const inf = int(2e9)
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	n, x := getI(), getI()
+	dp := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		dp[i] = make([]int, M+1)
+		for j := 0; j < M; j++ {
+			dp[i][j+1] = -inf
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		v, a, c := getI(), getI(), getI()
+		for j := x; j >= c; j-- {
+			dp[v-1][j] = max(dp[v-1][j], dp[v-1][j-c]+a)
+		}
+	}
+	for i := 0; i < 3; i++ {
+		for j := 0; j < x; j++ {
+			dp[i][j+1] = max(dp[i][j], dp[i][j+1])
+		}
+	}
+
+	idx := []int{0, 0, 0}
+	for i := 0; i < x; i++ {
+		if (dp[0][idx[0]] <= dp[1][idx[1]]) && (dp[0][idx[0]] <= dp[2][idx[2]]) {
+			idx[0]++
+		} else if (dp[1][idx[1]] <= dp[0][idx[0]]) && (dp[1][idx[1]] <= dp[2][idx[2]]) {
+			idx[1]++
+		} else {
+			idx[2]++
+		}
+	}
+	out(nmin(dp[0][idx[0]], dp[1][idx[1]], dp[2][idx[2]]))
+}

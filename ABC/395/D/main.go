5a0a40da30fdf966c9d1c8aaaffc7251ad036386
aruaru0 <aruaru0@gmail.com>
2025-03-05 10:16:08 +0900

    abc395-D

---
+++ ABC/395/D/main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func outSlice[T any](s []T) {
+	if len(s) == 0 {
+		return
+	}
+	for i := 0; i < len(s)-1; i++ {
+		fmt.Fprint(wr, s[i], " ")
+	}
+	fmt.Fprintln(wr, s[len(s)-1])
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, Q := getI(), getI()
+
+	p := make([]int, N)       // 鳩の場所
+	pos2idx := make([]int, N) // 巣のposからidxに直す
+	idx2pos := make([]int, N) // 巣のidxからposに直す
+
+	for i := 0; i < N; i++ {
+		p[i] = i
+		pos2idx[i] = i
+		idx2pos[i] = i
+	}
+
+	// 入力される巣の場所はposとする
+	for qi := 0; qi < Q; qi++ {
+		op := getI()
+		switch op {
+		case 1:
+			a, b := getI()-1, getI()-1
+			p[a] = pos2idx[b] // 巣のposからidxに直して格納
+		case 2:
+			a, b := getI()-1, getI()-1
+			pos2idx[a], pos2idx[b] = pos2idx[b], pos2idx[a] // 巣のidxを入れ替え
+			idx2pos[pos2idx[a]] = a                         // idx2posを更新する
+			idx2pos[pos2idx[b]] = b                         // idx2posを更新する
+		case 3:
+			a := getI() - 1
+			nb := idx2pos[p[a]] // 鳩がいるのは、idx番目なのでposに変換する
+			out(nb + 1)
+		}
+	}
+
+}

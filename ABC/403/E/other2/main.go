a5cd5cca895147361e3d68d219b9150939a15977
aruaru0 <aruaru0@gmail.com>
2025-05-01 11:56:02 +0900

    abc403-E update

---
+++ ABC/403/E/other2/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+/* ---------- I/O ユーティリティ ---------- */
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) { fmt.Fprintln(wr, x...) }
+
+func getI() int {
+	sc.Scan()
+	v, _ := strconv.Atoi(sc.Text())
+	return v
+}
+
+func getS() string { sc.Scan(); return sc.Text() }
+
+/* ---------- 汎用 ---------- */
+
+func lowerBound(a []int, x int) int {
+	return sort.Search(len(a), func(i int) bool { return a[i] >= x })
+}
+
+/* ----------------------------------------------------------------
+   Trie（ポインタ版）
+---------------------------------------------------------------- */
+
+type Node struct {
+	ch   []*Node // 子ノード
+	ng   bool    // X で”封印”されたら true
+	numY int     // ここに Y が何回置かれたか
+}
+
+type Trie struct {
+	root *Node
+	ans  int
+}
+
+func NewTrie() *Trie { return &Trie{root: &Node{ch: make([]*Node, 26)}} }
+
+// 文字列を挿入し、終端ノードへのポインタを返す
+func (t *Trie) Add(s string) *Node {
+	v := t.root
+	for _, c := range s {
+		pos := int(c - 'a')
+		if v.ch[pos] == nil {
+			v.ch[pos] = &Node{ch: make([]*Node, 26)}
+		}
+		v = v.ch[pos]
+	}
+	return v
+}
+
+func (t *Trie) Init() { t.ans = 0 } // 追加直後は全ノードが未使用なので 0 で十分
+
+// X クエリ
+func (t *Trie) AddX(v *Node) {
+	if v.ng {
+		return
+	}
+	v.ng = true
+	t.ans -= v.numY
+	for _, u := range v.ch {
+		if u != nil {
+			t.AddX(u)
+		}
+	}
+}
+
+// Y クエリ
+func (t *Trie) AddY(v *Node) {
+	if v.ng {
+		return
+	}
+	t.ans++
+	v.numY++
+}
+
+/* ---------- main ---------- */
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 0, 1<<20), math.MaxInt32)
+
+	q := getI()
+	tr := NewTrie()
+
+	// クエリを一旦ためる（典型的 AtCoder テク）
+	type query struct {
+		typ int
+		v   *Node
+	}
+	qs := make([]query, q)
+
+	for i := 0; i < q; i++ {
+		typ := getI()
+		s := getS()
+		v := tr.Add(s)
+		qs[i] = query{typ, v}
+	}
+	tr.Init()
+
+	for _, qu := range qs {
+		if qu.typ == 1 {
+			tr.AddX(qu.v)
+		} else {
+			tr.AddY(qu.v)
+		}
+		out(tr.ans)
+	}
+}

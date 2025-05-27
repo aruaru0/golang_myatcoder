f1a002c6593ccd85587dd21dca91db631c8bb8e9
aruaru0 <55325119+aruaru0@users.noreply.github.com>
2025-05-27 01:12:22 +0000

    abc407-F

---
+++ ABC/407/F/main.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+type nums interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~float32 | ~float64
+}
+
+type Ordered interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr |
+		~float32 | ~float64 | ~string
+}
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func outSlice[T any](s []T) {
+	if len(s) == 0 {
+		return
+	}
+	for i := 0; i < len(s)-1; i++ {
+		fmt.Fprint(wr, s[i], " ")
+	}
+	fmt.Fprintln(wr, s[len(s)-1])
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max[T Ordered](a, b T) T {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min[T Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin[T Ordered](a ...T) T {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax[T Ordered](a ...T) T {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin[T Ordered](a *T, b T) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax[T Ordered](a *T, b T) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub[T nums](a, b T) T {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs[T nums](a T) T {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound[T nums](a []T, x T) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound[T nums](a []T, x T) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type CartesianTree struct {
+	n, root int
+	l, r    []int
+}
+
+func NewCartesianTree(a []int) CartesianTree {
+	ct := CartesianTree{n: len(a)}
+	ct.l = make([]int, ct.n)
+	ct.r = make([]int, ct.n)
+	for i := 0; i < ct.n; i++ {
+		ct.l[i], ct.r[i] = -1, -1
+	}
+	st := make([]int, 0)
+	for i := 0; i < ct.n; i++ {
+		p := -1
+		for len(st) != 0 && a[st[len(st)-1]] < a[i] { // > min
+			j := st[len(st)-1]
+			st = st[:len(st)-1]
+			ct.r[j] = p
+			p = j
+		}
+		ct.l[i] = p
+		st = append(st, i)
+	}
+	for i := 0; i < len(st)-1; i++ {
+		ct.r[st[i]] = st[i+1]
+	}
+	ct.root = st[0]
+	return ct
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	n := getI()
+	a := getInts(n)
+
+	t := NewCartesianTree(a)
+	d := make([]int, n+2)
+
+	var f func(v int) int
+	f = func(v int) int {
+		if v == -1 {
+			return 0
+		}
+		l := f(t.l[v]) + 1
+		r := f(t.r[v]) + 1
+		d[0] += a[v]
+		d[l] -= a[v]
+		d[r] -= a[v]
+		d[l+r] += a[v]
+		return l + r - 1
+	}
+
+	f(t.root)
+
+	for i := 0; i <= n; i++ {
+		d[i+1] += d[i]
+	}
+	for i := 0; i <= n; i++ {
+		d[i+1] += d[i]
+	}
+	for i := 0; i < n; i++ {
+		out(d[i])
+	}
+}

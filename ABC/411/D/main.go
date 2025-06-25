a157aa39bd1a06f172d4014895748450eaf6acd8
aruaru0 <aruaru0@gmail.com>
2025-06-25 09:18:29 +0900

    abc411-D

---
+++ ABC/411/D/main.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func outSlice[T any](s []T) {
+	if len(s) == 0 {
+		return
+	}
+	for i := 0; i < len(s)-1; i++ {
+		fmt.Fprint(wr, s[i], " ")
+	}
+	fmt.Fprintln(wr, s[len(s)-1])
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+type operation struct {
+	t, p int
+	s    string
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+
+	_, Q := getI(), getI()
+
+	op := make([]operation, Q)
+	for qi := 0; qi < Q; qi++ {
+		op[qi].t, op[qi].p = getI(), getI()
+		if op[qi].t == 2 {
+			op[qi].s = getS()
+		}
+	}
+
+	ans := []byte{}
+	idx := 0
+	for qi := Q - 1; qi >= 0; qi-- {
+		switch op[qi].t {
+		case 1: // p <- server
+			if idx == op[qi].p {
+				idx = 0
+			}
+		case 2: // append p to string
+			if idx == op[qi].p {
+				for i := len(op[qi].s) - 1; i >= 0; i-- {
+					ans = append(ans, op[qi].s[i])
+				}
+			}
+		default: // server <- p
+			if idx == 0 {
+				idx = op[qi].p
+			}
+		}
+	}
+
+	txt := make([]byte, 0)
+	for i := len(ans) - 1; i >= 0; i-- {
+		txt = append(txt, ans[i])
+	}
+	out(string(txt))
+}

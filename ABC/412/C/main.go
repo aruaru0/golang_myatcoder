a34e122267686ece7031b612539823f0352fbb01
aruaru0 <aruaru0@gmail.com>
2025-07-01 09:01:07 +0900

    abc412-C

---
+++ ABC/412/C/main.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func outSlice[T any](s []T) {
+	if len(s) == 0 {
+		return
+	}
+	for i := 0; i < len(s)-1; i++ {
+		fmt.Fprint(wr, s[i], " ")
+	}
+	fmt.Fprintln(wr, s[len(s)-1])
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+func solve() {
+	n := getI()
+	s := getInts(n)
+
+	start, end := s[0], s[n-1]
+
+	// 条件が２つで成立するなら最小は２個
+	if start*2 >= end {
+		out(2)
+		return
+	}
+
+	// 重複をカットしてソート
+	m := make(map[int]bool)
+	for i := 0; i < n; i++ {
+		m[s[i]] = true
+	}
+	t := make([]int, 0)
+	for e := range m {
+		t = append(t, e)
+	}
+	sort.Ints(t)
+
+	// 個数を数える
+	cnt := 1
+	cur := start
+	for cur < end { // endを超えたら終了
+
+		// ２倍より小さいものがあれば最大のものを
+		pos := upperBound(t, cur*2) - 1
+		nxt := t[pos]
+		// 現在値よりnxtが小さい場合は終了
+		if nxt <= cur {
+			break
+		}
+		cur = nxt
+		cnt++
+	}
+
+	if cur >= end {
+		out(cnt)
+	} else {
+		out(-1)
+	}
+
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	T := getI()
+
+	for i := 0; i < T; i++ {
+		solve()
+	}
+}

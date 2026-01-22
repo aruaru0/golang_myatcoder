b47b3c9cede1b2e705f771f1b5d471bd8b2818a7
aruaru0 <aruaru0@gmail.com>
2026-01-22 09:29:27 +0900

    abc411-F

---
+++ ABC/441/F/main.go
@@ -0,0 +1,230 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func outSlice[T any](s []T) {
+	if len(s) == 0 {
+		return
+	}
+	for i := 0; i < len(s)-1; i++ {
+		fmt.Fprint(wr, s[i], " ")
+	}
+	fmt.Fprintln(wr, s[len(s)-1])
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getStrings(N int) []string {
+	ret := make([]string, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getS()
+	}
+	return ret
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+// 値を圧縮した配列を返す
+func compressArray(a []int) []int {
+	m := make(map[int]int)
+	for _, e := range a {
+		m[e] = 1
+	}
+	b := make([]int, 0)
+	for e := range m {
+		b = append(b, e)
+	}
+	sort.Ints(b)
+	for i, e := range b {
+		m[e] = i
+	}
+
+	ret := make([]int, len(a))
+	for i, e := range a {
+		ret[i] = m[e]
+	}
+	return ret
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, M := getI(), getI()
+
+	p := make([]int, N)
+	v := make([]int, N)
+	for i := 0; i < N; i++ {
+		p[i], v[i] = getI(), getI()
+	}
+
+	suf := make([][]int, N+1)
+	for i := 0; i <= N; i++ {
+		suf[i] = make([]int, M+1)
+	}
+
+	for i := N - 1; i >= 0; i-- {
+		for w := 0; w <= M; w++ {
+			suf[i][w] = suf[i+1][w]
+			if w >= p[i] {
+				suf[i][w] = max(suf[i][w], suf[i+1][w-p[i]]+v[i])
+			}
+		}
+	}
+
+	v_max := suf[0][M]
+
+	pre := make([]int, M+1)
+	ans := []byte{}
+
+	for i := 0; i < N; i++ {
+		// sel
+		mx := -1
+		rem := M - p[i]
+		for w := 0; w <= rem; w++ {
+			cur := pre[w] + suf[i+1][rem-w] + v[i]
+			mx = max(mx, cur)
+		}
+		// not sel
+		mx2 := -1
+		for w := 0; w <= M; w++ {
+			cur := pre[w] + suf[i+1][M-w]
+			mx2 = max(mx2, cur)
+		}
+
+		if mx < v_max {
+			ans = append(ans, 'C')
+		} else {
+			if mx2 < v_max {
+				ans = append(ans, 'A')
+			} else {
+				ans = append(ans, 'B')
+			}
+		}
+		for w := M; w >= p[i]; w-- {
+			pre[w] = max(pre[w], pre[w-p[i]]+v[i])
+		}
+	}
+	out(string(ans))
+
+}

8ae5cc56cb2e7b615ec8e860185a742ed0cc7389
aruaru0 <aruaru0@gmail.com>
2026-02-14 10:30:24 +0900

    awc0001-D

---
+++ ARC/0001/D/main.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func outSlice[T any](s []T) {
+	if len(s) == 0 {
+		return
+	}
+	for i := 0; i < len(s)-1; i++ {
+		fmt.Fprint(wr, s[i], " ")
+	}
+	fmt.Fprintln(wr, s[len(s)-1])
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getStrings(N int) []string {
+	ret := make([]string, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getS()
+	}
+	return ret
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+// 値を圧縮した配列を返す
+func compressArray(a []int) []int {
+	m := make(map[int]int)
+	for _, e := range a {
+		m[e] = 1
+	}
+	b := make([]int, 0)
+	for e := range m {
+		b = append(b, e)
+	}
+	sort.Ints(b)
+	for i, e := range b {
+		m[e] = i
+	}
+
+	ret := make([]int, len(a))
+	for i, e := range a {
+		ret[i] = m[e]
+	}
+	return ret
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, M, K := getI(), getI(), getI()
+	a := make([]int, N)
+	b := make([]int, N)
+	for i := 0; i < N; i++ {
+		a[i], b[i] = getI(), getI()
+	}
+
+	// dp[i][j] iまで見たときに、滞在費がjで最後の場所がKの場合の最大の利益
+	dp := make([][]int, N+1)
+	for i := 0; i <= N; i++ {
+		dp[i] = make([]int, M+1)
+	}
+	ans := -1
+	for i := 1; i <= N; i++ { // 街の番号
+		idx := i - 1
+		for k := 1; k <= K; k++ { // K個手前からやってきた
+			if i-k < 0 {
+				break
+			}
+			for j := 0; j < M; j++ {
+				if j+b[i-1] <= M { //　滞在費がMを超えない場合
+					dp[i][j+b[idx]] = max(dp[i][j+b[idx]], dp[i-k][j]+a[idx])
+					ans = max(ans, dp[i][j+b[idx]])
+				}
+			}
+		}
+	}
+
+	out(ans)
+}

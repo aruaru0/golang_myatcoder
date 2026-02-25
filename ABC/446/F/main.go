3f7fae667987954059fce28174fa1d4812a3f0f8
aruaru0 <aruaru0@gmail.com>
2026-02-25 10:06:11 +0900

    abc446-F

---
+++ ABC/446/F/main.go
@@ -0,0 +1,307 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func outSlice[T any](s []T) {
+	if len(s) == 0 {
+		return
+	}
+	for i := 0; i < len(s)-1; i++ {
+		fmt.Fprint(wr, s[i], " ")
+	}
+	fmt.Fprintln(wr, s[len(s)-1])
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getStrings(N int) []string {
+	ret := make([]string, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getS()
+	}
+	return ret
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+// 値を圧縮した配列を返す
+func compressArray(a []int) []int {
+	m := make(map[int]int)
+	for _, e := range a {
+		m[e] = 1
+	}
+	b := make([]int, 0)
+	for e := range m {
+		b = append(b, e)
+	}
+	sort.Ints(b)
+	for i, e := range b {
+		m[e] = i
+	}
+
+	ret := make([]int, len(a))
+	for i, e := range a {
+		ret[i] = m[e]
+	}
+	return ret
+}
+
+//
+// Disjoint Set Union: Union Find Tree
+//
+
+// DSU :
+type DSU struct {
+	parentOrSize []int
+	n            int
+}
+
+// newDsu :
+func newDsu(n int) *DSU {
+	var d DSU
+	d.n = n
+	d.parentOrSize = make([]int, n)
+	for i := 0; i < n; i++ {
+		d.parentOrSize[i] = -1
+	}
+	return &d
+}
+
+// Merge :
+func (d DSU) Merge(a, b int) int {
+	x, y := d.Leader(a), d.Leader(b)
+	if x == y {
+		return x
+	}
+	if -d.parentOrSize[x] < -d.parentOrSize[y] {
+		x, y = y, x
+	}
+	d.parentOrSize[x] += d.parentOrSize[y]
+	d.parentOrSize[y] = x
+	return x
+}
+
+// Same :
+func (d DSU) Same(a, b int) bool {
+	return d.Leader(a) == d.Leader(b)
+}
+
+// Leader :
+func (d DSU) Leader(a int) int {
+	if d.parentOrSize[a] < 0 {
+		return a
+	}
+	d.parentOrSize[a] = d.Leader(d.parentOrSize[a])
+	return d.parentOrSize[a]
+}
+
+// Size :
+func (d DSU) Size(a int) int {
+	return -d.parentOrSize[d.Leader(a)]
+}
+
+// Groups : original implement
+func (d DSU) Groups() [][]int {
+	m := make(map[int][]int)
+	for i := 0; i < d.n; i++ {
+		x := d.Leader(i)
+		if x < 0 {
+			m[i] = append(m[i], i)
+		} else {
+			m[x] = append(m[x], i)
+		}
+	}
+	ret := make([][]int, len(m))
+	idx := 0
+	for _, e := range m {
+		ret[idx] = make([]int, len(e))
+		copy(ret[idx], e)
+		idx++
+	}
+	return ret
+}
+
+type Pair struct {
+	u, v int
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	n, m := getI(), getI()
+
+	to := make([][]int, n)
+	ot := make([][]int, n)
+	for i := 0; i < m; i++ {
+		a, b := getI()-1, getI()-1
+		to[a] = append(to[a], b)
+		ot[b] = append(ot[b], a)
+
+	}
+
+	mark := make([]bool, n)
+	markCnt := 0
+	adj := make([]bool, n)
+	adjCnt := 0
+
+	q := []int{}
+
+	reach := func(v int) {
+		if mark[v] {
+			return
+		}
+		mark[v] = true
+		markCnt++
+		q = append(q, v)
+	}
+
+	for k := 0; k < n; k++ {
+		if adj[k] {
+			adjCnt--
+		}
+		if adj[k] || k == 0 {
+			reach(k)
+		}
+		for len(q) != 0 {
+			v := q[0]
+			q = q[1:]
+			for _, u := range to[v] {
+				if u <= k {
+					reach(u)
+				} else {
+					if adj[u] == false {
+						adj[u] = true
+						adjCnt++
+					}
+				}
+			}
+		}
+		if k+1 == markCnt {
+			out(adjCnt)
+		} else {
+			out(-1)
+		}
+	}
+
+}

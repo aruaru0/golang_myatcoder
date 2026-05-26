ad470bb58fd0ad707e1afa4a4d7e053ac587d435
aruaru0 <aruaru0@gmail.com>
2026-05-26 09:45:48 +0900

    abc459-D

---
+++ ABC/459/D/main.go
@@ -0,0 +1,257 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func outSlice[T any](s []T) {
+	if len(s) == 0 {
+		return
+	}
+	for i := 0; i < len(s)-1; i++ {
+		fmt.Fprint(wr, s[i], " ")
+	}
+	fmt.Fprintln(wr, s[len(s)-1])
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getStrings(N int) []string {
+	ret := make([]string, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getS()
+	}
+	return ret
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+// 値を圧縮した配列を返す
+func compressArray(a []int) []int {
+	m := make(map[int]int)
+	for _, e := range a {
+		m[e] = 1
+	}
+	b := make([]int, 0)
+	for e := range m {
+		b = append(b, e)
+	}
+	sort.Ints(b)
+	for i, e := range b {
+		m[e] = i
+	}
+
+	ret := make([]int, len(a))
+	for i, e := range a {
+		ret[i] = m[e]
+	}
+	return ret
+}
+
+type pqi struct{ cnt, v int }
+
+type priorityQueue []pqi
+
+func (pq priorityQueue) Len() int            { return len(pq) }
+func (pq priorityQueue) Swap(i, j int)       { pq[i], pq[j] = pq[j], pq[i] }
+func (pq priorityQueue) Less(i, j int) bool  { return pq[i].cnt > pq[j].cnt }
+func (pq *priorityQueue) Push(x interface{}) { *pq = append(*pq, x.(pqi)) }
+func (pq *priorityQueue) Pop() interface{} {
+	x := (*pq)[len(*pq)-1]
+	*pq = (*pq)[0 : len(*pq)-1]
+	return x
+}
+
+type moji struct {
+	c, cnt int
+}
+
+func solve() {
+	s := getS()
+	n := len(s)
+
+	m := make([]moji, 26)
+	for i := 0; i < 26; i++ {
+		m[i].c = i
+	}
+
+	mx := 0
+	for i := 0; i < n; i++ {
+		v := int(s[i] - 'a')
+		m[v].cnt++
+		mx = max(mx, m[v].cnt)
+	}
+
+	if mx > (n+1)/2 {
+		out("No")
+		return
+	} else {
+		out("Yes")
+	}
+
+	pq := priorityQueue{}
+	for e := range m {
+		if m[e].cnt != 0 {
+			heap.Push(&pq, pqi{m[e].cnt, m[e].c})
+		}
+	}
+
+	ans := []byte{}
+	prev := -1
+	for len(pq) != 0 {
+		t0 := heap.Pop(&pq).(pqi)
+		if len(pq) == 0 {
+			ans = append(ans, byte(t0.v+'a'))
+		} else {
+			t1 := heap.Pop(&pq).(pqi)
+			if prev == t0.v {
+				ans = append(ans, byte(t1.v+'a'))
+				t1.cnt--
+				if t1.cnt != 0 {
+					heap.Push(&pq, t1)
+				}
+				heap.Push(&pq, t0)
+				prev = t1.v
+			} else {
+				ans = append(ans, byte(t0.v+'a'))
+				t0.cnt--
+				if t0.cnt != 0 {
+					heap.Push(&pq, t0)
+				}
+				heap.Push(&pq, t1)
+				prev = t0.v
+			}
+		}
+		// out(pq, string(ans))
+	}
+	out(string(ans))
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	T := getI()
+	for i := 0; i < T; i++ {
+		solve()
+	}
+}

b876ae5f45ad9029488866d0dbdff00d672644e8
aruaru0 <aruaru0@gmail.com>
2026-06-13 10:40:57 +0900

    awc0015-E

---
+++ AWC/0015/E/main.go
@@ -0,0 +1,271 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var wr = bufio.NewWriter(os.Stdout)
+
+func out(x ...interface{}) {
+	fmt.Fprintln(wr, x...)
+}
+
+func outSlice[T any](s []T) {
+	if len(s) == 0 {
+		return
+	}
+	for i := 0; i < len(s)-1; i++ {
+		fmt.Fprint(wr, s[i], " ")
+	}
+	fmt.Fprintln(wr, s[len(s)-1])
+}
+
+func getI() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getF() float64 {
+	sc.Scan()
+	i, e := strconv.ParseFloat(sc.Text(), 64)
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func getInts(N int) []int {
+	ret := make([]int, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getI()
+	}
+	return ret
+}
+
+func getS() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func getStrings(N int) []string {
+	ret := make([]string, N)
+	for i := 0; i < N; i++ {
+		ret[i] = getS()
+	}
+	return ret
+}
+
+// min, max, asub, absなど基本関数
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// min for n entry
+func nmin(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = min(ret, e)
+	}
+	return ret
+}
+
+// max for n entry
+func nmax(a ...int) int {
+	ret := a[0]
+	for _, e := range a {
+		ret = max(ret, e)
+	}
+	return ret
+}
+
+func chmin(a *int, b int) bool {
+	if *a < b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func chmax(a *int, b int) bool {
+	if *a > b {
+		return false
+	}
+	*a = b
+	return true
+}
+
+func asub(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func lowerBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] >= x
+	})
+	return idx
+}
+
+func upperBound(a []int, x int) int {
+	idx := sort.Search(len(a), func(i int) bool {
+		return a[i] > x
+	})
+	return idx
+}
+
+// 値を圧縮した配列を返す
+func compressArray(a []int) []int {
+	m := make(map[int]int)
+	for _, e := range a {
+		m[e] = 1
+	}
+	b := make([]int, 0)
+	for e := range m {
+		b = append(b, e)
+	}
+	sort.Ints(b)
+	for i, e := range b {
+		m[e] = i
+	}
+
+	ret := make([]int, len(a))
+	for i, e := range a {
+		ret[i] = m[e]
+	}
+	return ret
+}
+
+type BIT struct {
+	v []int
+}
+
+func newBIT(n int) *BIT {
+	b := new(BIT)
+	b.v = make([]int, n)
+	return b
+}
+func (b BIT) sum(a int) int {
+	ret := 0
+	for i := a + 1; i > 0; i -= i & -i {
+		ret += b.v[i-1]
+	}
+	return ret
+}
+func (b BIT) rangeSum(x, y int) int {
+	if y == 0 {
+		return 0
+	}
+	y--
+	if x == 0 {
+		return b.sum(y)
+	} else {
+		return b.sum(y) - b.sum(x-1)
+	}
+}
+func (b BIT) add(a, w int) {
+	n := len(b.v)
+	for i := a + 1; i <= n; i += i & -i {
+		b.v[i-1] += w
+	}
+}
+
+// (累積和が k+1 以上になる最初のインデックス)
+func (b *BIT) getKth(k int) int {
+	n := len(b.v)
+	x := 0
+	w := 1
+
+	// n以下の最大の2の冪乗を求める
+	for w*2 <= n {
+		w *= 2
+	}
+
+	for w > 0 {
+		if x+w <= n && b.v[x+w-1] < k+1 {
+			k -= b.v[x+w-1]
+			x += w
+		}
+		w /= 2
+	}
+	return x
+}
+
+type Pair struct {
+	l, r int
+	idx  int
+}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer([]byte{}, math.MaxInt32)
+	// this template is new version.
+	// use getI(), getS(), getInts(), getF()
+	N, Q := getI(), getI()
+	p := getInts(N)
+	q := make([]Pair, Q)
+	for i := 0; i < Q; i++ {
+		q[i] = Pair{getI() - 1, getI() - 1, i}
+	}
+
+	sort.Slice(q, func(i, j int) bool {
+		return q[i].r < q[j].r
+	})
+
+	bit := newBIT(N)
+
+	l := make([]int, N+1)
+	for i := range l {
+		l[i] = -1
+	}
+
+	ans := make([]int, Q)
+	qi := 0
+	for i := 0; i < N; i++ {
+		v := p[i]
+		if l[v] != -1 {
+			bit.add(l[v], -1)
+		}
+		bit.add(i, 1)
+		l[v] = i
+
+		for qi < Q && q[qi].r == i {
+			v := q[qi]
+			ans[v.idx] = bit.rangeSum(v.l, v.r+1)
+			qi++
+		}
+	}
+
+	for i := 0; i < Q; i++ {
+		out(ans[i])
+	}
+}
